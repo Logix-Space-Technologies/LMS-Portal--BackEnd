8b626a05317b4b9b9a1154f58bf7fe117e4bbde4
Glenngt <glenn.mattackal@gmail.com>
2023-12-12 14:54:30 +0530

    LMSBE-2 Admin-login-API-Completed

--- app/models/admin.model.js
+++ app/models/admin.model.js
@@ -5,20 +5,49 @@ const Admin = function(admin){
     this.Password = admin.Password
 }
 
-Admin.create = (newAdmin, result) =>{
-    db.query("INSERT INTO admin SET ?", newAdmin,(err,res)=>{
+// Admin.create = (newAdmin, result) =>{
+//     db.query("INSERT INTO admin SET ?", newAdmin,(err,res)=>{
+
+//         if (err) {
+//             console.log("Error: ", err)
+//             result(err,null)
+//             return
+
+//         } else {
+//             console.log("Admin : ",{id:res.id, ...newAdmin})
+//             result(null, {id:res.id, ...newAdmin})
+            
+//         }
+//     })
+// }
+
+
+
+Admin.findByUserName = (username,result)=>{
+    db.query("SELECT * FROM admin WHERE userName = ?", username, (err,res)=>{
 
         if (err) {
-            console.log("Error: ", err)
-            result(err,null)
+            console.log("Error : ", err)
+            result(err, null)
             return
-
-        } else {
-            console.log("Admin : ",{id:res.id, ...newAdmin})
-            result(null, {id:res.id, ...newAdmin})
-            
         }
+
+        if (res.length) {
+            result(null, res[0])
+            return
+        }
+
+        result({kind : "not_found"}, null)
+
     })
 }
+
+
+
+
+
+
+
+
 
 module.exports = Admin
