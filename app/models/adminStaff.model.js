3f75d581a7247ca02001ce28d8054f7ccdd45335
DevNandNair <devnandnair2@gmail.com>
2023-12-13 21:47:32 +0530

    LMSBE-3 addAdminStaff API completed

--- app/models/adminStaff.model.js
+++ app/models/adminStaff.model.js
@@ -1,38 +1,60 @@
 const { response } = require("express")
 const db = require("../models/db")
 
-const AdminStaff = function(adminStaff){
+const AdminStaff = function (adminStaff) {
     this.AdStaffName = adminStaff.AdStaffName
     this.PhNo = adminStaff.PhNo
     this.Address = adminStaff.Address
     this.AadharNo = adminStaff.AadharNo
     this.Email = adminStaff.Email
     this.Password = adminStaff.Password
-    
+
 }
 
 
 
-AdminStaff.create = (newAdminStaff, result)=>{
-    if (newAdminStaff.AdStaffName!="" && newAdminStaff.AdStaffName != null) {
-        db.query("INSERT INTO admin_staff SET ?", newAdminStaff, (err,res)=>{
-            console.log(newAdminStaff)
+AdminStaff.create = (newAdminStaff, result) => {
+    if (newAdminStaff.AdStaffName != "" && newAdminStaff.AdStaffName != null) {
+        db.query("SELECT * FROM admin_staff WHERE Email=?", newAdminStaff.Email, (err, res) => {
             if (err) {
-                console.log("error: ", err)
-                result(err,null)
-                return
-                
+                console.log("error: ", err);
+                result(null, err);
+                return;
             } else {
-                console.log("Added Admin Staff: ",{id: res.id, ...newAdminStaff})
-                result(null,{id: res.id, ...newAdminStaff})
+                if (res.length > 0) {
+                    console.log("Email already exists");
+                    result(null, "Email already exists");
+                    return;
 
-                
+                } else {
+
+
+                    db.query("INSERT INTO admin_staff SET ?", newAdminStaff, (err, res) => {
+                        console.log(newAdminStaff)
+                        if (err) {
+                            console.log("error: ", err)
+                            result(err, null)
+                            return
+
+                        } else {
+                            console.log("Added Admin Staff: ", { id: res.id, ...newAdminStaff })
+                            result(null, { id: res.id, ...newAdminStaff })
+
+
+                        }
+                    })
+
+
+
+                }
+
             }
         })
-        
+
+
     } else {
-        response.json({"status":"Content cannot be empty!"})
-        
+        response.json({ "status": "Content cannot be empty!" })
+
     }
 }
 
