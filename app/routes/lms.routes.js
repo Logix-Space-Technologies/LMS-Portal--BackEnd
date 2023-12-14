04a680432901f5c897585904608050a0b8de10f6
bibint6@gmail.com <bibint6@gmail.com>
2023-12-14 15:50:33 +0530

    LMSBE-8 Test

--- app/routes/lms.routes.js
+++ app/routes/lms.routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const AdminController = require('../controllers/admin.controller')
 const CollegeController = require('../controllers/college.controller')
 const AdminStaffController = require('../controllers/adminStaff.controller')
-
+const ClgStaffController=require("../controllers/clgStaff.controller")
 // router.post("/", admin.adminRegister)
 router.post("/", AdminController.adminLogin)
 
@@ -12,5 +12,9 @@ router.post("/addCollege",CollegeController.collegeCreate)
 router.post("/viewCollege",CollegeController.viewCollege)
 
 router.post("/addAdminStaff",AdminStaffController.create)
+
+
+router.post("/addclgstaff",ClgStaffController.clgStaffCreate)
+
 
 module.exports = router
