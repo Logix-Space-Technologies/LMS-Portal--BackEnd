086ca27f56f1368efebc93af22aff4c738342470
Animesh Thomas <animeshthomas262@gmail.com>
2023-12-14 10:05:52 +0530

    removed

--- app/routes/lms.routes.js
+++ app/routes/lms.routes.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const AdminController = require('../controllers/admin.controller')
-const CollegeController = require('../controllers/college.controller')
+
 
 // router.post("/", admin.adminRegister)
-router.post("/", AdminController.adminLogin)
 
-router.post("/addCollege",CollegeController.collegeCreate)
-
-router.post("/viewCollege",CollegeController.viewCollege)
 
 
 module.exports = router
