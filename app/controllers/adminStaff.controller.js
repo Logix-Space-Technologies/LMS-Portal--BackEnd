ef502ec1392df382eae1cbabf079d928cf99c70d
DevNandNair <devnandnair2@gmail.com>
2023-12-15 17:06:30 +0530

    LMSBE-3 addAdminStaff API password validation pending

--- app/controllers/adminStaff.controller.js
+++ app/controllers/adminStaff.controller.js
@@ -14,43 +14,54 @@ exports.create = (request, response) => {
         Email: request.body.Email,
         Password: request.body.Password
 
-        
+
     });
 
     const token = request.body.token;
 
-
-    bcrypt.hash(adminstaff.Password, saltRounds, (err, hashedPassword) => {
-        if (err) {
-            response.json({ "status": err });
-        } else {
-            adminstaff.Password = hashedPassword;
+    const MIN_PASSWORD_LENGTH = 8
+    const MAX_PASSWORD_LENGTH = 12
 
 
-            if (adminstaff.AdStaffName != "" && adminstaff.AdStaffName != null) {
-                AdminStaff.create(adminstaff, (err, data) => {
-                    if (err) {
-                        response.json({ "status": err });
-                    } else {
-                        jwt.verify(token, "lmsapp", (error, decoded) => {
-                            if (decoded) {
-                                response.json({ "status": "success", "data": data });
-                            } else {
-                                response.json({ "status": "Unauthorized User!!" });
-                            }
-                        });
-                    }
+    if (adminstaff.Password.length >= MIN_PASSWORD_LENGTH && adminstaff.Password.length <= MAX_PASSWORD_LENGTH) {
 
-                });
-
+        bcrypt.hash(adminstaff.Password, saltRounds, (err, hashedPassword) => {
+            if (err) {
+                response.json({ "status": err });
             } else {
-                response.json({ "status": "Content cannot be empty." });
+                adminstaff.Password = hashedPassword;
+                if (adminstaff.AdStaffName != "" && adminstaff.AdStaffName != null) {
+                    AdminStaff.create(adminstaff, (err, data) => {
+                        if (err) {
+                            response.json({ "status": err });
+                        } else {
+                            jwt.verify(token, "lmsapp", (error, decoded) => {
+                                if (decoded) {
+                                    response.json({ "status": "success", "data": data });
+                                } else {
+                                    response.json({ "status": "Unauthorized User!!" });
+                                }
+                            });
+                        }
+
+                    });
+
+                } else {
+                    response.json({ "status": "Content cannot be empty." });
+                }
+
+
+
             }
+        });
 
 
+    } else {
+        response.json({ "status": "Password length should be between 8 and 12 characters!!" })
 
-        }
-    });
+    }
+
+
 };
 
 
--- app/models/adminStaff.model.js
+++ app/models/adminStaff.model.js
@@ -8,54 +8,64 @@ const AdminStaff = function (adminStaff) {
     this.AadharNo = adminStaff.AadharNo
     this.Email = adminStaff.Email
     this.Password = adminStaff.Password
-   
-    
+
+
 
 }
 
+const MIN_PASSWORD_LENGTH = 8
+const MAX_PASSWORD_LENGTH = 12
 
 
 AdminStaff.create = (newAdminStaff, result) => {
-    if (newAdminStaff.AdStaffName != "" && newAdminStaff.AdStaffName != null) {
-        db.query("SELECT * FROM admin_staff WHERE Email=?", newAdminStaff.Email, (err, res) => {
-            if (err) {
-                console.log("error: ", err);
-                result(err, null);
-                return;
-            } else {
-                if (res.length > 0) {
-                    console.log("Email already exists");
-                    result("Email already exists",null );
+    const admPassword = newAdminStaff.Password.length
+    if (admPassword >= MIN_PASSWORD_LENGTH && admPassword <= MAX_PASSWORD_LENGTH) {
+
+        if (newAdminStaff.AdStaffName != "" && newAdminStaff.AdStaffName != null) {
+            db.query("SELECT * FROM admin_staff WHERE Email=?", newAdminStaff.Email, (err, res) => {
+                if (err) {
+                    console.log("error: ", err);
+                    result(err, null);
                     return;
-
                 } else {
+                    if (res.length > 0) {
+                        console.log("Email already exists");
+                        result("Email already exists", null);
+                        return;
+
+                    } else {
 
 
-                    db.query("INSERT INTO admin_staff SET ?", newAdminStaff, (err, res) => {
-                        console.log(newAdminStaff)
-                        if (err) {
-                            console.log("error: ", err)
-                            result(err, null)
-                            return
+                        db.query("INSERT INTO admin_staff SET ?", newAdminStaff, (err, res) => {
+                            console.log(newAdminStaff)
+                            if (err) {
+                                console.log("error: ", err)
+                                result(err, null)
+                                return
 
-                        } else {
-                            console.log("Added Admin Staff: ", { id: res.id, ...newAdminStaff })
-                            result(null, { id: res.id, ...newAdminStaff })
-
-
-                        }
-                    })
+                            } else {
+                                console.log("Added Admin Staff: ", { id: res.id, ...newAdminStaff })
+                                result(null, { id: res.id, ...newAdminStaff })
 
 
+                            }
+                        })
+
+
+
+                    }
 
                 }
+            })
 
-            }
-        })
 
+        } else {
+            response.json({ "status": "Content cannot be empty!" })
+
+        }
 
     } else {
-        response.json({ "status": "Content cannot be empty!" })
+        response.json({ "status": "Password length should be between 8 and 12 characters!!" })
 
     }
 }
