2af981adf22de98a8cb472c5f4080d1e56cebcf9
DevNandNair <devnandnair2@gmail.com>
2023-12-18 09:39:12 +0530

    LMSBE-3 addAdminStaff API completed with all  bug fixed

--- app/models/adminStaff.model.js
+++ app/models/adminStaff.model.js
@@ -9,72 +9,39 @@ const AdminStaff = function (adminStaff) {
     this.Email = adminStaff.Email
     this.Password = adminStaff.Password
 
-
-
 }
 
-const MIN_PASSWORD_LENGTH = 8
-const MAX_PASSWORD_LENGTH = 12
 
 
 AdminStaff.create = (newAdminStaff, result) => {
-    const admPassword = newAdminStaff.Password.length
-    if (admPassword >= MIN_PASSWORD_LENGTH && admPassword <= MAX_PASSWORD_LENGTH) {
-
-        if (newAdminStaff.AdStaffName != "" && newAdminStaff.AdStaffName != null) {
-            db.query("SELECT * FROM admin_staff WHERE Email=?", newAdminStaff.Email, (err, res) => {
-                if (err) {
-                    console.log("error: ", err);
-                    result(err, null);
-                    return;
-                } else {
-                    if (res.length > 0) {
-                        console.log("Email already exists");
-                        result("Email already exists", null);
-                        return;
-
-                    } else {
-
-
-                        db.query("INSERT INTO admin_staff SET ?", newAdminStaff, (err, res) => {
-                            console.log(newAdminStaff)
-                            if (err) {
-                                console.log("error: ", err)
-                                result(err, null)
-                                return
-
-                            } else {
-                                console.log("Added Admin Staff: ", { id: res.id, ...newAdminStaff })
-                                result(null, { id: res.id, ...newAdminStaff })
-
-
-                            }
-                        })
-
-
-
-                    }
-
-                }
-            })
-
-
+    
+    db.query("SELECT * FROM admin_staff WHERE Email=?", newAdminStaff.Email, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
         } else {
-            response.json({ "status": "Content cannot be empty!" })
-
+            if (res.length > 0) {
+                console.log("Email already exists");
+                result("Email already exists", null);
+                return;
+            } else {
+                // Continue with the existing code for database insertion
+                db.query("INSERT INTO admin_staff SET ?", newAdminStaff, (err, res) => {
+                    console.log(newAdminStaff)
+                    if (err) {
+                        console.log("error: ", err)
+                        result(err, null)
+                        return
+                    } else {
+                        console.log("Added Admin Staff: ", { id: res.id, ...newAdminStaff })
+                        result(null, { id: res.id, ...newAdminStaff })
+                    }
+                })
+            }
         }
-
-    } else {
-        response.json({ "status": "Password length should be between 8 and 12 characters!!" })
-
-    }
-}
-
-
-
-
-
-
+    })
+};
 
 
 
