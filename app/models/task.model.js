a8f6dd256859e42de007b7e04c78f315777fe584
bibint6@gmail.com <bibint6@gmail.com>
2023-12-20 01:58:55 +0530

    LMSBE-22 Bug Fixed

--- app/models/task.model.js
+++ app/models/task.model.js
@@ -12,30 +12,45 @@ const Tasks = function (tasks) {
 };
 
 Tasks.taskCreate = (newTask, result) => {
-    db.query("SELECT * FROM task WHERE taskTitle=? AND batchId=?", [newTask.taskTitle, newTask.batchId], (err, res) => {
+    db.query("SELECT * FROM batches WHERE id=?", [newTask.batchId], (err, batchRes) => {
         if (err) {
-            console.error("Error checking existing task: ", err);
+            console.error("Error checking existing batch: ", err);
             result(err, null);
             return;
         } else {
-            if (res.length > 0) {
-                console.log("Task already exists.");
-                result("Task Title already exists.", null)
+            if (batchRes.length === 0) {
+                console.log("Batch does not exist.");
+                result("Batch ID does not exist.", null);
                 return;
-            } else {
-                db.query("INSERT INTO task SET ?", newTask, (err, res) => {
-                    if (err) {
-                        console.error("Error inserting task: ", err);
-                        result(err, null);
+            }
+
+            db.query("SELECT * FROM task WHERE taskTitle=? AND batchId=?", [newTask.taskTitle, newTask.batchId], (err, taskRes) => {
+                if (err) {
+                    console.error("Error checking existing task: ", err);
+                    result(err, null);
+                    return;
+                } else {
+                    if (taskRes.length > 0) {
+                        console.log("Task already exists.");
+                        result("Task Title already exists.", null);
                         return;
                     } else {
-                        console.log("Task inserted: ", { id: res.id, ...newTask });
-                        result(null, { id: res.id, ...newTask });
+                        db.query("INSERT INTO task SET ?", newTask, (err, insertRes) => {
+                            if (err) {
+                                console.error("Error inserting task: ", err);
+                                result(err, null);
+                                return;
+                            } else {
+                                console.log("Task inserted: ", { id: insertRes.insertId, ...newTask });
+                                result(null, { id: insertRes.insertId, ...newTask });
+                            }
+                        });
                     }
-                });
-            }
+                }
+            });
         }
     });
 };
+
 
 module.exports = Tasks;
