91deed6dedf31f4b570aac010faed073fdac3f9b
Sree1710 <sreelekshmisl1710@gmail.com>
2023-12-20 14:20:55 +0530

    test

--- app/controllers/batches.controller.js
+++ app/controllers/batches.controller.js
@@ -100,7 +100,7 @@ exports.batchDelete = (request, response) => {
 }
 
 exports.batchView = (request, response) => {
-    const batchToken = request.body.batchToken;
+    const batchToken = request.body.token;
     jwt.verify(batchToken, "lmsapp", (decoded, err) => {
         if (decoded) {
             Batches.batchView((err, data) => {
