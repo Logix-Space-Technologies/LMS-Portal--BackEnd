e9c9b9ee0688f1c7c0d9356efdd844054c6d7957
hakkimbinqamar <hakkimbinqamar@gmail.com>
2024-01-04 14:05:16 +0530

    LMSBE-120 initial committ

--- webapp-frontend/lms-app-frontend/src/components/Admin/AdminDashboard.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/AdminDashboard.jsx
@@ -4,11 +4,13 @@ import 'react-calendar/dist/Calendar.css';
 
 export const AdminDashboard = () => {
   
-  const [adminData, setAdminData]= useState(
+  const [collegeData, setCollegeData]= useState(
     []
   ) 
 
   
+
+
   return (
     <div className="container-xxl position-relative bg-white d-flex p-0">
       {/* Spinner Start */}
