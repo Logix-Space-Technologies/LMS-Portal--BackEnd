7bbb10294a50509dbfda23209fbd0964ee95f545
Sree1710 <sreelekshmisl1710@gmail.com>
2024-01-05 18:21:22 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/AdminStaff/AdminStaffLogin.jsx
+++ webapp-frontend/lms-app-frontend/src/components/AdminStaff/AdminStaffLogin.jsx
@@ -18,12 +18,20 @@ const AdminStaffLogin = () => {
     const readValue = () => {
         axios.post(apiUrl,inputField).then(
             (Response)=>{
-                if (Response.data.status == "success") {
-                    let token = Response.data.token
-                    sessionStorage.setItem("ustoken", token)
+                if (Response.data.status === "Success") {
+                    let admstafftoken = Response.data.token
+                    sessionStorage.setItem("admstaffLogintoken", admstafftoken)
                     alert(Response.data.status)  
                 } else {
-                    alert(Response.data.status)
+                    if (Response.data.status === "Validation failed" && Response.data.data.email) {
+                        alert(Response.data.data.email)
+                    } else {
+                        if (Response.data.status === "Validation failed" && Response.data.data.password) {
+                            alert(Response.data.data.password)
+                        } else {
+                            alert(Response.data.status)
+                        }
+                    }
                 }
             }
         )
