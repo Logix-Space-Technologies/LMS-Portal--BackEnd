79bdeb4be1e8197a723669d394fda9fe0c109c61
Animesh Thomas <animeshthomas262@gmail.com>
2024-01-11 12:46:50 +0530

    registration succesfull mail done for,adminstaff,collegestaff and college

--- app/controllers/student.controller.js
+++ app/controllers/student.controller.js
@@ -7,7 +7,6 @@ const Validator = require("../config/data.validate");
 const PDFDocument = require('pdfkit-table');
 const fs = require('fs');
 
-
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/');
@@ -123,7 +122,6 @@ exports.createStudent = (req, res) => {
                 password: hashedPassword
             });
 
-
             Student.create(newStudent, (err, data) => {
                 if (err) {
                     return res.json({ "status": err });
@@ -140,79 +138,7 @@ exports.createStudent = (req, res) => {
                         if (paymentErr) {
                             return res.json({ "status": paymentErr });
                         } else {
-                            //send email
-                            const mail = require('../../sendEmail');
-
-
-
-                            //content
-
-                            const participantName = newStudent.studName;
-                            const participantEmail = newStudent.studEmail;
-
-
-
-                            const emailHtmlContent = `
-                                    <!DOCTYPE html>
-                                    <html>
-                                    <head>
-                                        <style>
-                                            .container {
-                                                font-family: Arial, sans-serif;
-                                                max-width: 600px;
-                                                margin: 0 auto;
-                                                padding: 20px;
-                                                border: 1px solid #ddd;
-                                                border-radius: 5px;
-                                                background-color: #f9f9f9;
-                                            }
-                                            .header {
-                                                font-size: 24px;
-                                                color: #333;
-                                            }
-                                            .content {
-                                                font-size: 16px;
-                                                color: #555;
-                                            }
-                                            .footer {
-                                                font-size: 14px;
-                                                color: #999;
-                                                margin-top: 20px;
-                                                text-align: center;
-                                            }
-                                            .button {
-                                                display: inline-block;
-                                                padding: 10px 20px;
-                                                margin-top: 20px;
-                                                background-color: #007bff;
-                                                color: white;
-                                                text-decoration: none;
-                                                border-radius: 5px;
-                                                font-weight: bold;
-                                            }
-                                        </style>
-                                    </head>
-                                    <body>
-                                        <div class="container">
-                                            <div class="header">
-                                                Registration Successful!
-                                            </div>
-                                            <div class="content">
-                                                <p>Dear ${participantName},</p>
-                                                <p>Thank you for joining Linkurcodes. We are excited to have you on board! Your training will commence shortly, and we are looking forward to helping you enhance your skills.</p>
-                                                <p>For more details about the upcoming sessions, please use our mobile app or contact your college directly. Stay tuned for more updates and prepare to embark on a rewarding learning journey with us.</p>
-                                            </div>
-                                            <div class="footer">
-                                                Best regards,<br>
-                                                The Linkurcodes Team
-                                            </div>
-                                        </div>
-                                    </body>
-                                    </html>
-`;
-                            // email sending code
-                            mail.sendEmail(participantEmail, 'Welcome Aboard! Your Registration with Linkurcodes is Complete!', emailHtmlContent);
-
+                            
                             return res.json({ "status": "success", "data": data, "paymentData": paymentData });
                         }
                     });
@@ -613,16 +539,16 @@ exports.searchStudentsByAdmAndAdmstf = (request, response) => {
 exports.studRegViewBatch = (request, response) => {
     const collegeId = request.body.collegeId;
     Student.viewBatch(collegeId, (err, data) => {
-        if (err) {
-            response.json({ "status": err });
-        }
-        if (data.length === 0) {
-            response.json({ "status": "No Batch found!" });
-        } else {
-            response.json({ "status": "success", "data": data });
-        }
-    });
-
+      if (err) {
+        response.json({ "status": err });
+      }
+      if (data.length === 0) {
+        response.json({ "status": "No Batch found!" });
+      } else {
+        response.json({ "status": "success", "data": data });
+      }
+      });
+ 
 };
 
 exports.studregCollegeAllView = (request, response) => {
@@ -673,7 +599,7 @@ function generatePDF(data, callback) {
         margin: { top: 30, bottom: 30 },
     });
     doc.text('\n');
-
+    
 
     // Group data by batch
     const groupedData = groupDataByBatch(data);
@@ -691,8 +617,8 @@ function generatePDF(data, callback) {
             const students = groupedData[batchName];
 
             // Create table headers
-            const tableHeaders = ['Membership No', 'Name', 'College', 'Email', 'Dept', 'Course'];
-            const tableData = students.map(student => [student.membership_no, student.studName, student.collegeName, student.studEmail, student.studDept, student.course]);
+            const tableHeaders = ['Membership No','Name', 'College', 'Email', 'Dept', 'Course'];
+            const tableData = students.map(student => [student.membership_no,student.studName, student.collegeName, student.studEmail, student.studDept,student.course]);
 
             // Draw the table
             doc.table({
