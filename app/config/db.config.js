03f99ee12f4253f98e02514e9272dfd2dda355e4
anishpdm <anish.vilayil.s@gmail.com>
2024-01-12 19:07:56 +0530

    changed

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,10 @@
 require('dotenv').config({ path: '../../.env' });
 
+console.log("Host=>"+ process.env.HOST)
+console.log("DATABASEUSER=>"+ process.env.DATABASEUSER)
+console.log("DATABASEPASSWORD=>"+ process.env.DATABASEPASSWORD)
+console.log("DB=>"+ process.env.DB)
+
 module.exports = {
     HOST: process.env.HOST,
     USER: process.env.DBUSER,
