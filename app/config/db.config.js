0de39dcdaf3082152eb3133566b17d9a8e8598b7
anishpdm <anish.vilayil.s@gmail.com>
2024-01-12 18:52:38 +0530

    changed

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 require('dotenv').config({ path: '../../.env' });
 
 module.exports = {
-    HOST: process.env.HOST,
-    USER: process.env.USER,
-    PASSWORD: process.env.PASSWORD,
-    DB: process.env.DB
+    HOST: 'localhost',
+    USER: 'lmsadmin',
+    PASSWORD: 'Kerala0#86LinkUrCode_s86',
+    DB: 'lmsdb'
   };
 
