1628533255bd26f671c8b75bb38e274dbf49dc7e
anishpdm <anish.vilayil.s@gmail.com>
2024-01-14 08:46:25 +0530

    cors config

--- server.js
+++ server.js
@@ -7,11 +7,25 @@ const mysql = require('mysql2')
 const adminRoutes = require("./app/routes/lms.routes");
 const app = express();
 
+const allowedOrigins = ['https://lms.linkurcodes.com', 'http://localhost:3000'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200 // For legacy browser support
+};
+
+
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use("/api/lms", adminRoutes);
 
 
