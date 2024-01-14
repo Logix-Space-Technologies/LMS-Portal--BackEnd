01f3e06bc92d327625b6e3d36d0fd88a899fa008
Sree1710 <sreelekshmisl1710@gmail.com>
2024-01-14 13:22:02 +0530

    test

--- app/controllers/student.controller.js
+++ app/controllers/student.controller.js
@@ -719,7 +719,7 @@ exports.studentNotificationView = (request, response) => {
 };
 
 
-
+//student view session details
 exports.studRegViewSession = (request, response) => {
     const viewSessionToken = request.headers.token;
     const key = request.headers.key;
