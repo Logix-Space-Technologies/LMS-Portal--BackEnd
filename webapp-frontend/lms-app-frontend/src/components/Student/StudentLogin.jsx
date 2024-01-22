bd5e4ab828b049575460c59f25645cc1a7e037cc
hakkimbinqamar <hakkimbinqamar@gmail.com>
2024-01-22 11:38:46 +0530

    LMSBE-203 student view attendance completed

--- webapp-frontend/lms-app-frontend/src/components/Student/StudentLogin.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Student/StudentLogin.jsx
@@ -27,8 +27,8 @@ const StudentLogin = () => {
                     sessionStorage.setItem("studentkey", key);
                     sessionStorage.setItem("studentId", studId);
                     sessionStorage.setItem("studLoginToken", studtoken);
-                    navigate("/studentviewattendance")
-                    
+                    navigate("/studdashboard")
+                    navigate("/refundrequest")
                     
 
                 } else {
