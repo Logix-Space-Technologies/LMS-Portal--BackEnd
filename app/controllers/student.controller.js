cb1a08809c93ddc19b5a81170d9b2cad7d61202c
Sree1710 <sreelekshmisl1710@gmail.com>
2024-01-24 18:29:45 +0530

    test

--- app/controllers/student.controller.js
+++ app/controllers/student.controller.js
@@ -614,15 +614,24 @@ exports.generateListOfBatchWiseStudents = (request, response) => {
         if (decoded) {
             Student.generateAllBatchWiseList((err, data) => {
                 if (err) {
-                    response.json({ "status": err });
+                    return response.json({ "status": err });
                 } else {
                     generatePDF(data, (pdfPath) => {
-                        response.json({ "status": "success", "data": data, "pdfPath": pdfPath });
+                        response.setHeader('Content-Type', 'application/pdf');
+                        response.setHeader('Content-Disposition', 'attachment; filename=batch_wise_students_list.pdf');
+                        fs.createReadStream(pdfPath).pipe(response);
+
+                        // Delete the generated PDF after sending it
+                        fs.unlink(pdfPath, (unlinkError) => {
+                            if (unlinkError) {
+                                console.error("Error deleting PDF:", unlinkError);
+                            }
+                        });
                     });
                 }
             });
         } else {
-            response.json({ "status": "Unauthorized User!!" });
+            return response.json({ "status": "Unauthorized User!!" });
         }
     });
 }
