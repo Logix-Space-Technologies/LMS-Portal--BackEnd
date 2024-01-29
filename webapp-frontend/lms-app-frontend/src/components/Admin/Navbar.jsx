eee0e260b7f58bfa3a95d3615ca37026cce7be4d
Sree1710 <sreelekshmisl1710@gmail.com>
2024-01-29 09:50:35 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/Admin/Navbar.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/Navbar.jsx
@@ -2,45 +2,55 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Admin</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Admin</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/addAdminStaff">Add Admin Staff</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/addAdminStaff">Add Admin Staff</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/addcollege">Add College</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/addcollege">Add College</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/addcollegestaff">Add College Staff</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/addcollegestaff">Add College Staff</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/adminchangepassword">Change Password</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/adminchangepassword">Change Password</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/adminsearchadminstaff">Search Admin Staff</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/adminsearchadminstaff">Search Admin Staff</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/adminsearchcurriculum">Search Curriculum</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/adminsearchcurriculum">Search Curriculum</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/adminsearchtask">Search Tasks</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/adminsearchtask">Search Tasks</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/adminviewadstafflog">View AdStaffLog</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/adminviewadstafflog">View AdStaffLog</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/adminviewallclgstaff">View College Staff</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/adminviewallclgstaff">View College Staff</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/admdashboard">Admin Dashboard</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/admdashboard">Admin Dashboard</a>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Functionalities
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item">
                             <a href="/" className="dropdown-item">Log Out</a>
                         </li>
                     </ul>
