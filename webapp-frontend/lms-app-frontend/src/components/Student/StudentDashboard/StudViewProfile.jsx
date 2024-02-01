eca80236aebd0a5bcf548320b6faca1f44cecec9
Sree1710 <sreelekshmisl1710@gmail.com>
2024-02-01 13:26:10 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/Student/StudentDashboard/StudViewProfile.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Student/StudentDashboard/StudViewProfile.jsx
@@ -33,7 +33,6 @@ const StudViewProfile = () => {
                     setStudData(response.data.data)
                     console.log(response.data.data)
                 } else {
-                    alert("Session Expired!! \n Login Again!!!")
                     logOut()
                     navigate("/studentLogin")
                 }
