a705da9ef2ffbd8518a17bac45c2e1165ef0621d
Animesh Thomas <animeshthomas262@gmail.com>
2024-02-07 15:32:45 +0530

    bug fixed

--- app/models/attendence.model.js
+++ app/models/attendence.model.js
@@ -85,7 +85,7 @@ Attendence.studentViewAttendance = (studentId, result) => {
 };
 
 Attendence.studentViewSessionWiseAttendance = (studentId, sessionId, result) => {
-    db.query("SELECT sd.sessionName, sd.date, CASE WHEN a.status = 0 THEN 'Absent' WHEN a.status = 1 THEN 'Present' ELSE 'Unknown' END AS attendance_status FROM attendence a JOIN sessiondetails sd ON a.sessionId = sd.id JOIN student s ON s.id = a.studId WHERE sd.cancelStatus = 0 AND s.id = ? AND a.sessionId = ? AND sd.date <= CURRENT_DATE()", [studentId, sessionId], (err, res) => {
+    db.query("SELECT sd.sessionName, sd.date, CASE WHEN a.status = 0 THEN 'Absent' WHEN a.status = 1 THEN 'Present' ELSE 'Unknown' END AS attendence_status FROM attendence a JOIN sessiondetails sd ON a.sessionId = sd.id JOIN student s ON s.id = a.studId WHERE sd.cancelStatus = 0 AND s.id = ? AND a.sessionId = ? AND sd.date <= CURRENT_DATE()", [studentId, sessionId], (err, res) => {
         if (err) {
             console.log("error:", err);
             result(err, null);
