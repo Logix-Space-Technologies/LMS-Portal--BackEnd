6e3c5f195f8769b64980c2ccd1371203fd44fa7e
Glenngt <glenn.mattackal@gmail.com>
2024-02-19 18:13:47 +0530

    LMSBE-323 Send Notification Bug Fix completed

--- webapp-frontend/lms-app-frontend/src/components/Admin/AdminSendNotifications.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/AdminSendNotifications.jsx
@@ -1,51 +1,125 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import '../../config/config';
 import axios from 'axios';
 
 const AdminSendNotification = () => {
     const initialNotificationData = {
-        batchId: '',
-        message: '',
-        sendby: '',
-        title: ''
+        "collegeId": "",
+        "batchId": "",
+        "message": "",
+        "sendby": "",
+        "title": ""
     };
 
     const [notificationData, setNotificationData] = useState(initialNotificationData);
-    const [errorMessage, setErrorMessage] = useState('');
-    const [successMessage, setSuccessMessage] = useState('');
+    const [outputField, setOutputField] = useState([])
+    const [batches, setBatches] = useState([])
+    const [key, setKey] = useState('');
+
+    const apiUrl2 = global.config.urls.api.server + "/api/lms/viewallcolleges";
+    const batchUrl = global.config.urls.api.server + "/api/lms/adminviewbatch";
+    const apiUrl = global.config.urls.api.server + "/api/lms/sendNotification";
 
     const handleChange = (event) => {
         setNotificationData({ ...notificationData, [event.target.name]: event.target.value });
     };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setErrorMessage('');
-        setSuccessMessage('');
+    const getData = () => {
+        let currentKey = sessionStorage.getItem("admkey");
+        let token = sessionStorage.getItem("admtoken");
+        if (currentKey !== 'lmsapp') {
+            currentKey = sessionStorage.getItem("admstaffkey");
+            token = sessionStorage.getItem("admstaffLogintoken");
+            setKey(currentKey); // Update the state if needed
+        }
+        let axiosConfig2 = {
+            headers: {
+                'content-type': 'application/json;charset=UTF-8',
+                'Access-Control-Allow-Origin': '*',
+                "token": token,
+                "key": currentKey
+            }
+        };
+        axios.post(apiUrl2, {}, axiosConfig2).then(
+            (response) => {
+                setOutputField(response.data.data)
+                console.log(response.data.data)
+            }
+        )
+    }
 
-        const apiUrl = global.config.urls.api.server + "/api/lms/sendNotification";
+    const getBatches = (collegeId) => {
+        let currentKey = sessionStorage.getItem("admkey");
+        let token = sessionStorage.getItem("admtoken");
+        if (currentKey !== 'lmsapp') {
+            currentKey = sessionStorage.getItem("admstaffkey");
+            token = sessionStorage.getItem("admstaffLogintoken");
+            setKey(currentKey); // Update the state if needed
+        }
+        let axiosConfig3 = {
+            headers: {
+                'content-type': 'application/json;charset=UTF-8',
+                'Access-Control-Allow-Origin': '*',
+                "token": token,
+                "key": currentKey
+            }
+        };
+        console.log(collegeId)
+        axios.post(batchUrl, { collegeId }, axiosConfig3).then((response) => {
+            setBatches(response.data)
+            console.log(response.data)
+        })
+    }
+
+    const handleCollegeChange = (event) => {
+        const selectedCollegeId = event.target.value;
+        setNotificationData(prevState => ({ ...prevState, collegeId: selectedCollegeId }))
+        getBatches(selectedCollegeId);
+    }
+
+    const handleSubmit = async (event) => {
+        let currentKey = sessionStorage.getItem("admkey");
+        let token = sessionStorage.getItem("admtoken");
+        let sendby;
+        if (currentKey !== 'lmsapp') {
+            currentKey = sessionStorage.getItem("admstaffkey");
+            token = sessionStorage.getItem("admstaffLogintoken");
+            setKey(currentKey); // Update the state if needed
+        }
+        if (currentKey === 'lmsapp') {
+            sendby = 0
+        } else {
+            sendby = sessionStorage.getItem("admstaffId")
+        }
+        event.preventDefault();
         const axiosConfig = {
             headers: {
                 'content-type': 'application/json;charset=UTF-8',
                 "Access-Control-Allow-Origin": "*",
-                "token": sessionStorage.getItem("admtoken"),
-                "key": sessionStorage.getItem("admkey")
+                "token": token,
+                "key": currentKey
             }
         };
+        let data = {
+            "batchId": notificationData.batchId,
+            "message": notificationData.message,
+            "sendby": sendby,
+            "title": notificationData.title
+        }
 
         try {
-            const response = await axios.post(apiUrl, notificationData, axiosConfig);
-
+            const response = await axios.post(apiUrl, data, axiosConfig);
+            console.log(notificationData)
             if (response.data.status === 'Success') {
-                setSuccessMessage(response.data.message);
+                alert(response.data.message)
                 // Reset the text fields to their initial empty state
                 setNotificationData(initialNotificationData);
             } else {
-                setErrorMessage(response.data.message || 'Error sending notification');
+                alert(response.data.message)
             }
         } catch (error) {
-            setErrorMessage(error.message || 'Error sending notification');
+            alert(error.message)
         }
     };
 
@@ -101,55 +175,67 @@ const AdminSendNotification = () => {
         }
     };
 
+    useEffect(() => { getData() }, [])
+
+    useEffect(() => { setKey(sessionStorage.getItem("admkey") || '') }, []);
+
     return (
         <div style={styles.container}>
             <Navbar />
             <h2>Send Notification</h2>
-            {errorMessage && <p style={styles.errorMessage}>{errorMessage}</p>}
-            {successMessage && <p style={styles.successMessage}>{successMessage}</p>}
             <div style={styles.card}>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label style={styles.label}>Batch ID:</label>
-                        <input
-                            type="text"
-                            name="batchId"
-                            value={notificationData.batchId}
-                            onChange={handleChange}
-                            style={styles.input}
-                        />
-                    </div>
-                    <div>
-                        <label style={styles.label}>Message:</label>
-                        <textarea
-                            name="message"
-                            value={notificationData.message}
-                            onChange={handleChange}
-                            style={styles.textarea}
-                        />
-                    </div>
-                    <div>
-                        <label style={styles.label}>Sender:</label>
-                        <input
-                            type="text"
-                            name="sendby"
-                            value={notificationData.sendby}
-                            onChange={handleChange}
-                            style={styles.input}
-                        />
-                    </div>
-                    <div>
-                        <label style={styles.label}>Title:</label>
-                        <input
-                            type="text"
-                            name="title"
-                            value={notificationData.title}
-                            onChange={handleChange}
-                            style={styles.input}
-                        />
-                    </div>
-                    <button type="submit" style={styles.button}>Send Notification</button>
-                </form>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label htmlFor="collegeId" className="form-label">
+                        College Name <span className="text-danger">*</span>
+                    </label>
+                    <select
+                        name="collegeId"
+                        value={notificationData.collegeId}
+                        onChange={handleCollegeChange}
+                        id="collegeId"
+                        className="form-control">
+                        <option value="">Select</option>
+                        {outputField.map((value) => {
+                            return <option value={value.id}> {value.collegeName} </option>
+                        })}
+                    </select>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label htmlFor="batchName" className="form-label">
+                        Batch Name <span className="text-danger">*</span>
+                    </label>
+                    <select
+                        name="batchId"
+                        id="batchId"
+                        className="form-control"
+                        value={notificationData.batchId}
+                        onChange={handleChange}>
+                        <option value="">Select</option>
+                        {batches.data && batches.data.map((value) => {
+                            return <option value={value.id}> {value.batchName} </option>;
+                        })}
+                    </select>
+                </div>
+                <div>
+                    <label style={styles.label}>Title:</label>
+                    <input
+                        type="text"
+                        name="title"
+                        value={notificationData.title}
+                        onChange={handleChange}
+                        style={styles.input}
+                    />
+                </div>
+                <div>
+                    <label style={styles.label}>Message:</label>
+                    <textarea
+                        name="message"
+                        value={notificationData.message}
+                        onChange={handleChange}
+                        style={styles.textarea}
+                    />
+                </div>
+                <button type="submit" onClick={handleSubmit} style={styles.button}>Send Notification</button>
             </div>
         </div>
     );
