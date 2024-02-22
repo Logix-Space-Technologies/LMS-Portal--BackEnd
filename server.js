1e4e587aa973e8e76a24fba6ea3634044c63cfb3
anishpdm <anish.vilayil.s@gmail.com>
2024-02-22 13:01:40 +0530

    final

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mysql = require('mysql2');
 const adminRoutes = require("./app/routes/lms.routes");
 const app = express();
 
-const allowedOrigins = ['https://lms.linkurcodes.com', 'http://localhost:3000'];
+const allowedOrigins = ['https://lms.linkurcodes.com', 'http://localhost:3000', 'http://localhost:64561'];
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -16,16 +16,22 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  optionsSuccessStatus: 200 // For legacy browser support
+  optionsSuccessStatus: 200, // For legacy browser support
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  allowedHeaders: "Content-Type,Authorization",
 };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
-app.use("/api/lms", adminRoutes);
 
-// Handle preflight requests
+// Enable CORS for all routes
+app.use(cors(corsOptions));
+
+// Allow preflight requests for all routes
 app.options("*", cors(corsOptions));
+
+app.use("/api/lms", adminRoutes);
 
 app.listen(3030, '0.0.0.0', () => {
   console.log("Server is running on port 3030.");
