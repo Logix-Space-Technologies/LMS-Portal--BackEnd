fbf3ff0864ec3d610e8542430151ea633e1c5c4c
Sree1710 <sreelekshmisl1710@gmail.com>
2024-02-22 19:54:49 +0530

    bug fix

--- webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewAllBatch.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewAllBatch.jsx
@@ -109,7 +109,6 @@ const AdminViewAllBatch = () => {
                     {/* Table headers */}
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" className="px-6 py-3">Batch Id</th>
                             <th scope="col" className="px-6 py-3">College Name</th>
                             <th scope="col" className="px-6 py-3">Batch Name</th>
                             <th scope="col" className="px-6 py-3">Reg Start Date</th>
@@ -130,7 +129,6 @@ const AdminViewAllBatch = () => {
                         {/* Table rows */}
                         {currentBatches.length > 0 ? currentBatches.map((value, index) => (
                             <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <td className="px-6 py-4">{value.id}</td>
                                 <td className="px-6 py-4">{value.collegeName}</td>
                                 <td className="px-6 py-4">{value.batchName}</td>
                                 <td className="px-6 py-4">{new Date(value.regStartDate).toLocaleDateString()}</td>
