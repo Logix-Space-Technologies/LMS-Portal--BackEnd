385330215c4418cedbb2437927867563c815c8c1
Sree1710 <sreelekshmisl1710@gmail.com>
2024-02-27 19:29:06 +0530

    bug fix

--- webapp-frontend/lms-app-frontend/src/components/Admin/AdminDashboard/AdminHeader.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/AdminDashboard/AdminHeader.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const AdminHeader = () => {
-
-    const navigate = useNavigate()
-
     const logout = () => {
        sessionStorage.clear()
     }
