60448e3307c132c6d29e0239d79f87e4f799158a
hakkimbinqamar <hakkimbinqamar@gmail.com>
2024-02-28 11:25:04 +0530

    LMSBE-348 student registration bug partially fixed

--- webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffDashboard/CollegeStaffHeader.jsx
+++ webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffDashboard/CollegeStaffHeader.jsx
@@ -38,7 +38,12 @@ const CollegeStaffHeader = () => {
 
     const logOut = () => {
         sessionStorage.clear()
+        navigate('/clgStafflogin');
     }
+
+    const handleLogoutConfirm = () => {
+        logOut();
+    };
 
     useEffect(() => { getData() }, [])
     return (
@@ -56,11 +61,34 @@ const CollegeStaffHeader = () => {
                         </Link>
                         <div className="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                             <Link to="/clgstaffchangepassword" className="dropdown-item">Change Password</Link>
-                            <Link to="/clgStafflogin" onClick={logOut} className="dropdown-item">Log Out</Link>
+                            <Link to="/" className="dropdown-item" data-bs-toggle="modal" data-bs-target="#deleteConfirmationModal">
+                                Log Out
+                            </Link>
                         </div>
                     </div>
                 </div>
             </nav>
+            <div className="modal fade" id="deleteConfirmationModal" tabIndex="-1" aria-labelledby="deleteConfirmationModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="deleteConfirmationModalLabel">Logout Confirmation</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            Are you sure you want to Logout?
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+                                Close
+                            </button>
+                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={handleLogoutConfirm}>
+                                Logout
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
