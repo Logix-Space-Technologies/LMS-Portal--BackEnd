e5a6011a39d3b0ac66ca24e0a519915d5f2610bf
Sree1710 <sreelekshmisl1710@gmail.com>
2024-02-29 18:37:23 +0530

    bug fix

--- webapp-frontend/lms-app-frontend/src/components/AdminStaff/AdminStaffViewSubmittedTask.jsx
+++ webapp-frontend/lms-app-frontend/src/components/AdminStaff/AdminStaffViewSubmittedTask.jsx
@@ -138,7 +138,7 @@ const AdminStaffViewSubmittedTask = () => {
                 <AdmStaffNavBar />
                 <br />
                 <strong>Admin Staff View Submitted Tasks</strong><br /><br />
-                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <div className="relative overflow-x shadow-md sm:rounded-lg">
                     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
