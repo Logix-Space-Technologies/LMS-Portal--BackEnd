04824f599f52fdd04627332646b311a70a48f764
Sree1710 <sreelekshmisl1710@gmail.com>
2024-02-29 12:07:33 +0530

    bug fix

--- webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewAllStud.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewAllStud.jsx
@@ -116,7 +116,7 @@ const AdminViewAllStud = () => {
 
                 <div></div>
             </div>
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <div className="relative overflow-x shadow-md sm:rounded-lg">
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
