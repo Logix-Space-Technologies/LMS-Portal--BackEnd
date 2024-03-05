4c0c7dbf342c519f679cd854c64189e18a34872d
Sree1710 <sreelekshmisl1710@gmail.com>
2024-03-05 19:51:01 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewAllBatches.jsx
+++ webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewAllBatches.jsx
@@ -9,6 +9,14 @@ const CollegeStaffViewBatch = () => {
   const [loading, setLoading] = useState(true);
   const [studentCount, setStudentCount] = useState(0);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [batchPerPage] = useState(10); // Number of batches per page
+
+  const rangeSize = 5; // Number of pages to display in the pagination
+  const lastPage = Math.ceil(batches.length / batchPerPage); // Calculate the total number of pages
+  let startPage = Math.floor((currentPage - 1) / rangeSize) * rangeSize + 1; // Calculate the starting page for the current range
+  let endPage = Math.min(startPage + rangeSize - 1, lastPage); // Calculate the ending page for the current range
+
   const apiUrl = global.config.urls.api.server + "/api/lms/collegeStaffViewBatch";
   const apiUrl2 = global.config.urls.api.server + "/api/lms/generatePdf";
   const apiUrl3 = global.config.urls.api.server + "/api/lms/generateAttendancePdf"
@@ -148,8 +156,20 @@ const CollegeStaffViewBatch = () => {
     sessionStorage.setItem("clgstaffbatchId", id)
   }
 
+  // Logic for displaying current students
+  const indexOfLastBatch = currentPage * batchPerPage;
+  const indexOfFirstBatch = indexOfLastBatch - batchPerPage;
+  const currentBatch = batches ? batches.slice(indexOfFirstBatch, indexOfLastBatch) : [];
+
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  // Calculate total pages
+  const totalPages = Math.ceil(batches.length / batchPerPage);
+
   useEffect(() => { fetchBatches() }, []);
-  
+
 
   return (
     <div>
@@ -173,10 +193,10 @@ const CollegeStaffViewBatch = () => {
                   {loading ? (
                     <div className="col-12 text-center">Loading...</div>
                   ) : (
-                    batches.length === 0 ? (
+                    currentBatch.length === 0 ? (
                       <div className="col-12 text-center">No batches found!</div>
                     ) : (
-                      batches.map((batch, index) => {
+                      currentBatch.map((batch, index) => {
                         return (
                           <div key={index} className="col-12">
                             <div className="card">
@@ -215,6 +235,37 @@ const CollegeStaffViewBatch = () => {
                       })
                     )
                   )}
+                  <div className="flex items-center justify-between bg-white px-6 py-4 sm:px-6">
+                    <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                      <div>
+                        <p className="text-sm text-gray-700">
+                          Showing <span className="font-medium">{indexOfFirstBatch + 1}</span> to <span className="font-medium">{indexOfLastBatch > batches.length ? batches.length : indexOfLastBatch}</span> of <span className="font-medium">{batches.length}</span> results
+                        </p>
+                      </div>
+                      <div>
+                        <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                          <button onClick={() => currentPage > 1 && paginate(currentPage - 1)} className={`relative inline-flex items-center px-2 py-2 text-sm font-medium ${currentPage === 1 ? 'cursor-not-allowed text-gray-500' : 'text-gray-700 hover:bg-gray-50'} disabled:opacity-50`} disabled={currentPage === 1}>
+                            <span className="sr-only">Previous</span>
+                            <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                              <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
+                            </svg>
+                          </button>
+                          {/* Dynamically generate Link components for each page number */}
+                          {Array.from({ length: endPage - startPage + 1 }, (_, index) => (
+                            <button key={startPage + index} onClick={() => paginate(startPage + index)} className={`relative inline-flex items-center px-4 py-2 text-sm font-medium ${currentPage === startPage + index ? 'bg-indigo-600 text-white' : 'text-gray-700 hover:bg-gray-50'}`}>
+                              {startPage + index}
+                            </button>
+                          ))}
+                          <button onClick={() => currentPage < totalPages && paginate(currentPage + 1)} className={`relative inline-flex items-center px-2 py-2 text-sm font-medium ${currentPage === totalPages ? 'cursor-not-allowed text-gray-500' : 'text-gray-700 hover:bg-gray-50'} disabled:opacity-50`} disabled={currentPage === totalPages}>
+                            <span className="sr-only">Next</span>
+                            <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                              <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+                            </svg>
+                          </button>
+                        </nav>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
