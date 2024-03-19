756b2240e07e86bd6982e4c2645bb4cb3b53ba98
Animesh Thomas <animeshthomas262@gmail.com>
2024-03-19 21:17:50 +0530

    firebase keys added to env

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -4,10 +4,21 @@ const Admin = require("../models/admin.model");
 const Validator = require('../config/data.validate')
 const { AdminStaffLog, logAdminStaff } = require("../models/adminStaffLog.model")
 const firebaseAdmin = require('firebase-admin')
-const serviceAccount = require('../config/firebase_key/serviceaccount.json');
 
 firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount)
+    credential: firebaseAdmin.credential.cert({
+        type: process.env.TYPE,
+        project_id: process.env.PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), 
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: process.env.AUTH_URI,
+        token_uri: process.env.TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+        universe_domain: process.env.UNIVERSE_DOMAIN
+    })
 });
 
 exports.sendNotifications = (request, response) => {
