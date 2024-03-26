ed480080ec8d5d54792b3c4d71cd7f0488e00871
Sree1710 <sreelekshmisl1710@gmail.com>
2024-03-26 14:07:40 +0530

    LMSBE- 419 Bug Fix

--- app/controllers/clgStaff.controller.js
+++ app/controllers/clgStaff.controller.js
@@ -640,17 +640,16 @@ exports.clgStaffViewTask = (request, response) => {
       const sessionId = request.body.sessionId
       CollegeStaff.viewTask(sessionId, (err, data) => {
         if (err) {
-          response.json({ "status": err })
+          return response.json({ "status": err })
         }
         if (data.length === 0) {
-
-          response.json({ "status": "No task found" })
+          return response.json({ "status": "No task found" })
         } else {
-          response.json({ "status": "success", "data": data })
+          return response.json({ "status": "success", "data": data })
         }
       })
     } else {
-      response.json({ "status": "Unauthorized User!!" })
+      return response.json({ "status": "Unauthorized User!!" })
     }
   })
 }
