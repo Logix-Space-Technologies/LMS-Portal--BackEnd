4071ff4425e0264c67f8b1af2571e6f15a4376b0
Sree1710 <sreelekshmisl1710@gmail.com>
2024-03-27 20:45:22 +0530

    Bug Fix

--- webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewSession.jsx
+++ webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewSession.jsx
@@ -142,7 +142,7 @@ const CollegeStaffViewSession = () => {
     }
 
     function isSpecialDomain(venueLink) {
-        const domains = ["meet.google.com", "zoom.us", "youtube.com", "vimeo.com"];
+        const domains = ["meet.google.com", "zoom.us", "youtube.com", "vimeo.com", "teams.microsoft.com"];
         return domains.some(domain => venueLink.includes(domain));
     }
 
