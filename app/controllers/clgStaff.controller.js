127e2a0060962999ef8725be47eee93dfb746955
Sree1710 <sreelekshmisl1710@gmail.com>
2024-03-29 17:42:38 +0530

    Bug Fix

--- app/controllers/clgStaff.controller.js
+++ app/controllers/clgStaff.controller.js
@@ -97,7 +97,6 @@ exports.clgStaffCreate = (request, response) => {
       // });
       const clgStaffToken = request.headers.token
       key = request.headers.key;
-      console.log(clgStaffToken)
       jwt.verify(clgStaffToken, key, (err, decoded) => {
         if (decoded) {
           const profilePic = request.file ? request.file.filename : null
@@ -164,15 +163,14 @@ exports.clgStaffCreate = (request, response) => {
             return response.json({ "status": "Validation failed", "data": validationErrors });
           }
 
-
-
+          let formattedPhoneNumber = /^(\+91\s?|91\s?)/.test(request.body.phNo) ? request.body.phNo.replace(/^(\+91\s?|91\s?)/, '') : request.body.phNo;
 
           const clgstaff = new CollegeStaff({
 
             collegeId: request.body.collegeId,
             collegeStaffName: request.body.collegeStaffName,
             email: request.body.email,
-            phNo: request.body.phNo,
+            phNo: formattedPhoneNumber,
             aadharNo: request.body.aadharNo,
             clgStaffAddress: request.body.clgStaffAddress,
             profilePic: imageUrl,
@@ -346,11 +344,14 @@ exports.collegeStaffUpdate = (req, res) => {
             }
 
             const profilePic = req.file ? req.file.filename : null;
+
+            let formattedPhoneNumber = /^(\+91\s?|91\s?)/.test(req.body.phNo) ? req.body.phNo.replace(/^(\+91\s?|91\s?)/, '') : req.body.phNo;
+
             const clgstaff = new CollegeStaff({
               id: req.body.id,
               collegeId: req.body.collegeId,
               collegeStaffName: req.body.collegeStaffName,
-              phNo: req.body.phNo,
+              phNo: formattedPhoneNumber,
               clgStaffAddress: req.body.clgStaffAddress,
               profilePic: imageUrl,
               department: req.body.department,
