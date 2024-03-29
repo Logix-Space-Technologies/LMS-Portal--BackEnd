9ebe5681ac1d5e029bc85a47f1c2fe230b50566f
Sree1710 <sreelekshmisl1710@gmail.com>
2024-03-29 13:51:31 +0530

    Bug Fix

--- webapp-frontend/lms-app-frontend/src/components/AdminStaff/AdminStaffViewAllMaterial.jsx
+++ webapp-frontend/lms-app-frontend/src/components/AdminStaff/AdminStaffViewAllMaterial.jsx
@@ -108,6 +108,7 @@ const AdminStaffViewAllMaterial = () => {
                     <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
+                                <th scope="col" className="px-6 py-3">S/N</th>
                                 <th scope="col" className="px-6 py-3">College Name</th>
                                 <th scope="col" className="px-6 py-3">Batch Name</th>
                                 <th scope="col" className="px-6 py-3">File Name</th>
@@ -117,11 +118,13 @@ const AdminStaffViewAllMaterial = () => {
                                 <th scope="col" className="px-6 py-3">Added Date</th>
                                 <th scope="col" className="px-6 py-3"></th>
                                 <th scope="col" className="px-6 py-3"></th>
+                                <th scope="col" className="px-6 py-3"></th>
                             </tr>
                         </thead>
                         <tbody>
                             {currentMaterial.map((value, index) => (
                                 <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={index}>
+                                    <td className="px-6 py-4">{calculateSerialNumber(index)}</td>
                                     <td className="px-6 py-4">{value.collegeName}</td>
                                     <td className="px-6 py-4">{value.batchName}</td>
                                     <td className="px-6 py-4">{value.fileName}</td>
