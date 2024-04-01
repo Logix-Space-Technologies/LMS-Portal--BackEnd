5c729fc71ea2358fcd27e1ac2cb35ffa8a962e91
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-01 11:03:33 +0530

    Bug Fix

--- app/models/student.model.js
+++ app/models/student.model.js
@@ -271,6 +271,9 @@ Student.findByEmail = (Email, result) => {
             } else if (verifyRes.length === 0) {
                 console.log("Student Does Not Exist")
                 return result("Student Does Not Exist", null)
+            } else if (verifyRes[0].length === "0") {
+                console.log("Password Is Default Password. Please Change.")
+                return result("Password Is Default Password. Please Change.", null)
             } else {
                 db.query("SELECT * FROM student WHERE BINARY studEmail = ? AND isVerified = 1", [Email],
                     (err, res) => {
