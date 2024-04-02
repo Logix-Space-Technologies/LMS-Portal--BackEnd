1cf87f3965a180f56c8964112d332c299d04f02f
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-02 18:39:13 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/Admin/AdminAddTrainer.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/AdminAddTrainer.jsx
@@ -155,13 +155,11 @@ const AdminAddTrainer = () => {
                     const statusCode = error.response.status;
 
                     if (statusCode === 400) {
-                        console.log("Status 400:", error.response.data);
                         setTimeout(() => {
                             alert(error.response.data.status)
                         }, 500)
                         // Additional logic for status 400
                     } else if (statusCode === 500) {
-                        console.log("Status 500:", error.response.data);
                         setTimeout(() => {
                             alert(error.response.data.status)
                         }, 500)
@@ -172,17 +170,14 @@ const AdminAddTrainer = () => {
                         }, 500)
                     }
                 } else if (error.request) {
-                    console.log(error.request);
                     setTimeout(() => {
                         alert(error.request);
                     }, 500)
                 } else if (error.message) {
-                    console.log('Error', error.message);
                     setTimeout(() => {
                         alert('Error', error.message);
                     }, 500)
                 } else {
-                    console.log(error.config);
                     setTimeout(() => {
                         alert(error.config);
                     }, 500)
