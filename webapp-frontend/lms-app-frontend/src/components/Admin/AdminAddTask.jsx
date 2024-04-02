c1ec1361c0f12451e98cfe451e7c305e315baf0d
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-02 18:37:01 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/Admin/AdminAddTask.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/AdminAddTask.jsx
@@ -276,13 +276,11 @@ const AdminAddTask = () => {
                     const statusCode = error.response.status;
 
                     if (statusCode === 400) {
-                        console.log("Status 400:", error.response.data);
                         setTimeout(() => {
                             alert(error.response.data.status)
                         }, 500)
                         // Additional logic for status 400
                     } else if (statusCode === 500) {
-                        console.log("Status 500:", error.response.data);
                         setTimeout(() => {
                             alert(error.response.data.status)
                         }, 500)
@@ -293,17 +291,14 @@ const AdminAddTask = () => {
                         }, 500)
                     }
                 } else if (error.request) {
-                    console.log(error.request);
                     setTimeout(() => {
                         alert(error.request);
                     }, 500)
                 } else if (error.message) {
-                    console.log('Error', error.message);
                     setTimeout(() => {
                         alert('Error', error.message);
                     }, 500)
                 } else {
-                    console.log(error.config);
                     setTimeout(() => {
                         alert(error.config);
                     }, 500)
