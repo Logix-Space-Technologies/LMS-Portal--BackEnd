fe07bf8946d51e7de493bbb53a75b6c2abc463e2
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-06 14:59:03 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewRefundRequests.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewRefundRequests.jsx
@@ -601,7 +601,7 @@ const AdminViewRefundRequests = () => {
                             )}
                           </td>
                         </tr>
-                      }) : <td colSpan="10" className="text-dark border-b border-[#E8E8E8] bg-[#F3F6FF] dark:bg-dark-3 dark:border-dark dark:text-dark-7 py-5 px-2 text-center text-base font-medium">
+                      }) : <td colSpan="14" className="text-dark border-b border-[#E8E8E8] bg-[#F3F6FF] dark:bg-dark-3 dark:border-dark dark:text-dark-7 py-5 px-2 text-center text-base font-medium">
                         No Refund Requests Found !!!
                       </td>}
                     </tbody>
