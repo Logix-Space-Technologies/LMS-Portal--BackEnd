6ef6847454a71c815fc5e6f38b4c699388ae6aba
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-06 16:22:22 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/Student/RefundRequest.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Student/RefundRequest.jsx
@@ -5,13 +5,13 @@ import { Link, useNavigate } from 'react-router-dom';
 
 const RefundRequestForm = () => {
     const [inputField, setInputField] = useState({
-        studId: '',
-        reason: '',
-        accountNo: '',
-        IFSCCode: '',
-        bankName: '',
-        branchName: '',
-        upiId: ''
+        studId: "",
+        reason: "",
+        accountNo: "",
+        IFSCCode: "",
+        bankName: "",
+        branchName: "",
+        upiId: ""
     });
 
     const [errors, setErrors] = useState({});
