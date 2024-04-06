d08a2d14afcde06b04571e32079f543c491a1542
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-06 14:26:17 +0530

    test

--- app/models/clgStaff.model.js
+++ app/models/clgStaff.model.js
@@ -262,7 +262,7 @@ CollegeStaff.collegeStaffChangePassword = (college_staff, result) => {
             const hashedOldPassword = res[0].password;
             let clgstaffid = res[0].id;
             if (bcrypt.compareSync(college_staff.oldPassword, hashedOldPassword)) {
-                const updateCollegeStaffPasswordQuery = "UPDATE college_Staff SET password = ?, pwdUpdateStatus = 1 WHERE BINARY email = ? AND deleteStatus = 0 AND isActive = 1 ";
+                const updateCollegeStaffPasswordQuery = "UPDATE college_staff SET password = ?, pwdUpdateStatus = 1 WHERE BINARY email = ? AND deleteStatus = 0 AND isActive = 1 ";
                 const hashedNewPassword = bcrypt.hashSync(college_staff.newPassword, 10);
                 db.query(updateCollegeStaffPasswordQuery, [hashedNewPassword, college_staff.email], (updateErr) => {
                     if (updateErr) {
@@ -297,7 +297,7 @@ CollegeStaff.collegeStaffForgotPassword = (college_staff, result) => {
 
         const clgstaffData = clgstaff[0];
 
-        const updateCollegeStaffPasswordQuery = `UPDATE college_Staff SET password = ?, pwdUpdateStatus = 1 WHERE BINARY email = ? AND deleteStatus = 0 AND isActive = 1 `;
+        const updateCollegeStaffPasswordQuery = `UPDATE college_staff SET password = ?, pwdUpdateStatus = 1 WHERE BINARY email = ? AND deleteStatus = 0 AND isActive = 1 `;
 
         bcrypt.hash(college_staff.password, 10, (err, hashedNewPassword) => {
             if (err) {
