744617656dc4822c3ff4ef8db0b45e3f918697da
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-08 10:57:51 +0530

    test

--- app/controllers/refund.controller.js
+++ app/controllers/refund.controller.js
@@ -44,7 +44,7 @@ exports.createRefundRequest = (request, response) => {
                             let admNo = result[0].admNo;
                             let collegeName = result[0].collegeName;
                             let batchName = result[0].batchName;
-                            let addedDate = result[0].addedDate.toLocaleDateString('en-IN', { timeZone: 'Asia/Kolkata', day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'  });
+                            let addedDate = result[0].addedDate.toLocaleDateString('en-IN', { timeZone: 'Asia/Kolkata', day: '2-digit', month: '2-digit', year: 'numeric' });
                             let requestedDate = new Date().toLocaleDateString('en-IN', { timeZone: 'Asia/Kolkata', day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' });
                             refundRequestConfirmationHtmlContent = mailContents.refundRequestConfirmationHtmlContent(studName, requestedDate, addedDate, data.remainingPaymentPeriod);
                             refundRequestConfirmationTextContent = mailContents.refundRequestConfirmationTextContent(studName, requestedDate, addedDate, data.remainingPaymentPeriod)
