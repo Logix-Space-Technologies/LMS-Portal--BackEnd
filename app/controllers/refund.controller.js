ee3e808a6fde3394305b4b46f7889d07d69e8127
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-09 19:24:04 +0530

    test

--- app/controllers/refund.controller.js
+++ app/controllers/refund.controller.js
@@ -279,14 +279,14 @@ exports.rejectRefundRequest = (request, response) => {
                     console.log(err);
                     return response.json({ "status": err })
                 } else {
-                    db.query("SELECT * FROM `refund` WHERE `id` = ?", [refundrejectionId], (err, res) => {
+                    db.query("SELECT * FROM `refund` WHERE `id` = ? AND cancelStatus = 1", [refundrejectionId], (err, res) => {
                         if (err) {
                             console.log(err)
                         } else {
                             console.log(res[0].studId)
                             let rejectedStudId = res[0].studId;
                             let rejectedDate = new Date().toLocaleDateString('en-IN', { timeZone: 'Asia/Kolkata', day: '2-digit', month: '2-digit', year: 'numeric' });
-                            db.query("SELECT r.id AS refundId, s.* FROM student s JOIN refund r ON r.studId = s.id WHERE s.id = ? AND s.deleteStatus = 0 AND s.isActive = 1 AND r.cancelStatus = 0", [rejectedStudId], (rejectErr, rejectRes) => {
+                            db.query("SELECT r.id AS refundId, s.* FROM student s JOIN refund r ON r.studId = s.id WHERE s.id = ? AND s.deleteStatus = 0 AND s.isActive = 1 AND r.cancelStatus = 1", [rejectedStudId], (rejectErr, rejectRes) => {
                                 if (rejectErr) {
                                     console.log(rejectErr)
                                 } else {
