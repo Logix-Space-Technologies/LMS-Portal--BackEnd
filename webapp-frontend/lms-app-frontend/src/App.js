31c9a8a13089a463fa337376ea53c06c4fb2e66a
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-10 21:47:23 +0530

    test

--- webapp-frontend/lms-app-frontend/src/App.js
+++ webapp-frontend/lms-app-frontend/src/App.js
@@ -54,7 +54,6 @@ import AdminViewAllTrainers from './components/Admin/AdminViewAllTrainers';
 import StudNavBar from './components/Student/StudNavBar';
 import AdminAddBatch from './components/Admin/AdminAddBatch';
 import AdminViewAllBatch from './components/Admin/AdminViewAllBatch';
-import AdminSearchStudent from './components/Admin/AdminSearchStudent';
 import AdminViewCollegeStaffLog from './components/Admin/AdminViewCollegeStaffLog';
 import AdminAddSession from './components/Admin/AdminAddSession';
 import AdminSearchCollege from './components/Admin/AdminSearchCollege';
@@ -147,7 +146,6 @@ function App() {
           <Route path='/adminviewalltrainers' element={<AdminViewAllTrainers/>}/>
           <Route path='/adminaddbatch' element={<AdminAddBatch/>}/>
           <Route path='/adminviewallbatches' element={<AdminViewAllBatch/>}/>
-          <Route path='/adminsearchstudent' element={<AdminSearchStudent/>}/>
           <Route path='/adminviewCollegeStaffLog' element={<AdminViewCollegeStaffLog/>}/>
           <Route path='/adminaddsession' element={<AdminAddSession/>}/>
           <Route path='/adminSearchClg' element={<AdminSearchCollege/>}/>
