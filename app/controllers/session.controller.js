ddb333027c3dd77e8a0b6afe89c2a97ab3b7ca18
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-14 15:04:55 +0530

    test

--- app/controllers/session.controller.js
+++ app/controllers/session.controller.js
@@ -469,6 +469,7 @@ exports.deleteSession = (request, response) => {
 // Code for Searching the Session
 exports.searchSession = (request, response) => {
     const SessionSearchQuery = request.body.SessionSearchQuery
+    const collegeId = request.body.collegeId
     const SessionSearchToken = request.headers.token
     const key = request.headers.key;
 
@@ -477,7 +478,7 @@ exports.searchSession = (request, response) => {
             if (!SessionSearchQuery) {
                 return response.json({ "status": "Search Item is required." })
             }
-            Session.searchSession(SessionSearchQuery, (err, data) => {
+            Session.searchSession(collegeId, SessionSearchQuery, (err, data) => {
                 if (err) {
                     return response.json({ "status": err })
                 } else {
--- app/models/session.model.js
+++ app/models/session.model.js
@@ -249,10 +249,10 @@ Session.deleteSession = (sessionId, result) => {
 
 
 // Code for Searching the Session
-Session.searchSession = (search, result) => {
+Session.searchSession = (collegeId, search, result) => {
     const searchTerm = '%' + search + '%'
-    db.query("SELECT c.collegeName, s.id, b.batchName, s.sessionName, s.date, s.time, s.type, s.remarks, s.venueORlink, t.trainerName, s.attendenceCode, CASE WHEN cancelStatus = 0 THEN 'ACTIVE' WHEN cancelStatus = 1 THEN 'CANCELLED' ELSE 'unknown' END AS cancelStatus FROM sessiondetails s JOIN batches b ON s.batchId = b.id JOIN trainersinfo t ON s.trainerId = t.id JOIN college c ON b.collegeId = c.id WHERE s.isActive = 1 AND s.deleteStatus = 0  AND c.isActive = 1 AND c.deleteStatus = 0 AND  (b.batchName LIKE ? OR c.collegeName LIKE  ? OR t.trainerName LIKE ? OR s.sessionName LIKE ? ) ORDER BY s.addedDate DESC",
-        [searchTerm, searchTerm, searchTerm, searchTerm],
+    db.query("SELECT c.collegeName, s.id, b.batchName, s.sessionName, s.date, s.time, s.type, s.remarks, s.venueORlink, t.trainerName, s.attendenceCode, CASE WHEN cancelStatus = 0 THEN 'ACTIVE' WHEN cancelStatus = 1 THEN 'CANCELLED' ELSE 'unknown' END AS cancelStatus FROM sessiondetails s JOIN batches b ON s.batchId = b.id JOIN trainersinfo t ON s.trainerId = t.id JOIN college c ON b.collegeId = c.id WHERE c.id = ? AND s.isActive = 1 AND s.deleteStatus = 0  AND c.isActive = 1 AND c.deleteStatus = 0 AND  (b.batchName LIKE ? OR c.collegeName LIKE  ? OR t.trainerName LIKE ? OR s.sessionName LIKE ? ) ORDER BY s.addedDate DESC",
+        [collegeId, searchTerm, searchTerm, searchTerm, searchTerm],
         (err, res) => {
             if (err) {
                 console.log("Error : ", err)
