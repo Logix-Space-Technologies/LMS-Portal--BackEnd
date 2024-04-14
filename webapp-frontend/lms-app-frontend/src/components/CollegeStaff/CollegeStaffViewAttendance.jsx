b3fe82328a5fef7fe33d99471d10cb8cadf2ebf8
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-14 14:51:44 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewAttendance.jsx
+++ webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewAttendance.jsx
@@ -23,16 +23,16 @@ const CollegeStaffViewAttendance = () => {
 
     const getData = () => {
         const data = { "sessionId": sessionStorage.getItem("viewattendanceid") };
-        let currentKey = sessionStorage.getItem("admkey");
-        let token = sessionStorage.getItem("admtoken");
-        // Check and assign correct key and token based on conditions
-        if (currentKey !== 'lmsapp' && currentKey === 'lmsappadmstaff' && currentKey !== 'lmsappclgstaff') {
-            currentKey = sessionStorage.getItem("admstaffkey");
-            token = sessionStorage.getItem("admstaffLogintoken");
-        } else if (currentKey !== 'lmsapp' && currentKey !== 'lmsappadmstaff' && currentKey !== 'lmsappclgstaff') {
-            currentKey = sessionStorage.getItem("clgstaffkey");
-            token = sessionStorage.getItem("clgstaffLogintoken");
-        }
+
+        // Retrieve key and token from sessionStorage without providing the key
+        let currentKey, token;
+        Object.entries(sessionStorage).forEach(([key, value]) => {
+            if (key.includes('key')) {
+                currentKey = value;
+            } else if (key.includes('token')) {
+                token = value;
+            }
+        });
 
         // Update the state with the current key
         setKey(currentKey);
@@ -66,6 +66,7 @@ const CollegeStaffViewAttendance = () => {
     };
 
 
+
     // Logic for displaying current students
     const indexOfLastStudent = currentPage * attendancePerPage;
     const indexOfFirstStudent = indexOfLastStudent - attendancePerPage;
@@ -94,7 +95,7 @@ const CollegeStaffViewAttendance = () => {
             {key !== 'lmsappclgstaff' && key === 'lmsapp' ? <Navbar /> : <AdmStaffNavBar />}
             <div className="flex justify-between items-center mt-8 ml-4 mb-4">
                 {key === 'lmsapp' ? <h2 className="text-lg font-bold">Admin View Attendance</h2> : (key === 'lmsappclgstaff' ? <h2 className="text-lg font-bold">College Staff View Attendance</h2> : <h2 className="text-lg font-bold">Admin Staff View Attendance</h2>)}
-                <button type='button' onClick={()=> navigate(-1)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{ marginRight: '20px' }}>Back</button>
+                <button type='button' onClick={() => navigate(-1)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{ marginRight: '20px' }}>Back</button>
             </div>
             {loading ? <div className="col-12 text-center">Loading...</div> : <div className="relative overflow-x-auto shadow-md sm:rounded-lg"><table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead>
