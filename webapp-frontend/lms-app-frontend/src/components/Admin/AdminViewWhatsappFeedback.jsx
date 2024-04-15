6ef7f7dff9425f2269985d6501f211992d7ce821
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-15 14:21:00 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewWhatsappFeedback.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewWhatsappFeedback.jsx
@@ -97,7 +97,7 @@ const AdminViewWhatsappFeedback = () => {
                                     return (
                                         <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                             <td className="px-6 py-4">{calculateSerialNumber(index)}</td>
-                                            <td className="px-6 py-4">{value.studId}</td>
+                                            <td className="px-6 py-4">{value.membership_no}</td>
                                             <td className="px-6 py-4">{value.studName}</td>
                                             <td className="px-6 py-4">{value.msgId}</td>
                                             <td className="px-6 py-4">{value.message}</td>
