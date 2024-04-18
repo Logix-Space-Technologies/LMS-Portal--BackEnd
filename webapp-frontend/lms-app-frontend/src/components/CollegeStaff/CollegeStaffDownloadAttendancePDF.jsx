7030b3c820c0040437eb6909f4508a75930b0984
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-18 12:38:01 +0530

    LMSBE-455 test

--- webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffDownloadAttendancePDF.jsx
+++ webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffDownloadAttendancePDF.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import ClgStaffNavbar from './ClgStaffNavbar';
 import '../../config/config'
+import html2pdf from 'html2pdf.js';
+import axios from 'axios';
 
 const CollegeStaffDownloadAttendancePDF = () => {
     const pdfContentRef = useRef(null);
@@ -125,7 +127,7 @@ const CollegeStaffDownloadAttendancePDF = () => {
                                         <div className="flex space-x-4">
                                             <button onClick={() => navigate(-1)} className="btn bg-gray-500 text-white px-4 py-2 rounded-md">Back</button>
                                         </div>
-                                        <strong style={{ textAlign: "center", fontSize: "24px" }}>Download Preview Of Session-Wise Attendance List</strong>
+                                        <strong style={{ textAlign: "center", fontSize: "24px" }}>Download Preview Of Session Attendance List</strong>
                                         <div>
                                             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{ marginRight: "30px" }} onClick={generatePDF}>Download PDF</button>
                                         </div>
@@ -133,8 +135,8 @@ const CollegeStaffDownloadAttendancePDF = () => {
                                     <div></div><div></div>
                                     <div ref={pdfContentRef}>
                                         <img width="200px" src='/logo.png' alt="" />
-                                        <p style={{ textAlign: "center", fontSize: "22px", fontWeight: 'bold', marginBottom: '10px', textDecoration: "underline" }}>Batch-Wise Attendance List Of Students</p>
-                                        <p style={{ textAlign: "center", fontSize: "20px", fontWeight: 'bold', textDecoration: "underline" }}>{batchName}</p>
+                                        <p style={{ textAlign: "center", fontSize: "22px", fontWeight: 'bold', marginBottom: '10px', textDecoration: "underline" }}>Session Attendance List Of Students</p>
+                                        <p style={{ textAlign: "center", fontSize: "20px", fontWeight: 'bold', textDecoration: "underline" }}>Batch Name: {batchName}</p>
                                         <br />
                                         {Object.keys(groupedData).map(sessionName => {
                                             // Extract attendanceDate for the session
