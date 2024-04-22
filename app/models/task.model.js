399ae799472fa35427b54ec27378b46e37d2b76f
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-22 19:11:38 +0530

    LMSBE-475 clgstaff search student bug fix

--- app/models/task.model.js
+++ app/models/task.model.js
@@ -245,10 +245,10 @@ Tasks.searchTasks = (sessionId, searchString, result) => {
         })
 }
 
-Tasks.collegeStaffSearchTasks = (searchKey, collegeId, result) => {
+Tasks.collegeStaffSearchTasks = (searchKey, sessionId, result) => {
     const searchString = '%' + searchKey + '%'
-    db.query("SELECT DISTINCT c.collegeName, b.batchName, t.taskTitle, t.taskDesc, t.taskType, t.taskFileUpload, t.totalScore, t.dueDate, t.addedDate FROM task t JOIN batches b ON t.batchId= b.id JOIN college_staff cs ON b.collegeId = cs.collegeId JOIN college c ON b.collegeId = c.id WHERE t.deleteStatus = 0 AND t.isActive = 1 AND b.deleteStatus = 0 AND b.isActive = 1 AND cs.deleteStatus = 0 AND cs.isActive = 1 AND cs.collegeId = ? AND (t.taskTitle LIKE ? OR t.taskDesc LIKE ? OR t.taskType LIKE ? OR b.batchName LIKE ?) ORDER BY t.dueDate DESC",
-        [collegeId, searchString, searchString, searchString, searchString],
+    db.query("SELECT DISTINCT s.sessionName, t.batchId, t.id, t.taskTitle, t.taskDesc, t.taskType, t.taskFileUpload, t.totalScore, CASE WHEN t.dueDate < CURRENT_DATE() THEN 'Past Due Date' ELSE t.dueDate END AS dueDate, t.addedDate FROM task t LEFT JOIN sessiondetails s ON s.id = t.sessionId WHERE t.deleteStatus = 0 AND t.isActive = 1 AND s.deleteStatus = 0 AND s.isActive = 1 AND s.id = ? AND (t.taskType LIKE ? OR t.taskDesc LIKE ? OR t.taskTitle LIKE ?) ORDER BY dueDate DESC;",
+        [sessionId, searchString, searchString, searchString, searchString],
         (err, res) => {
             if (err) {
                 console.log("error: ", err);
