aeb973c625942a9dec723d12bba4cef0808c075f
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-22 18:12:16 +0530

    LMSBE-475 bug fix

--- webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewWhatsappFeedback.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewWhatsappFeedback.jsx
@@ -78,6 +78,8 @@ const AdminViewWhatsappFeedback = () => {
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" className="px-6 py-3">S/N</th>
+                                <th scope="col" className="px-6 py-3">College Name</th>
+                                <th scope="col" className="px-6 py-3">Batch Name</th>
                                 <th scope="col" className="px-6 py-3">Membership No.</th>
                                 <th scope="col" className="px-6 py-3">Student Name</th>
                                 <th scope="col" className="px-6 py-3">Message Id</th>
@@ -97,6 +99,8 @@ const AdminViewWhatsappFeedback = () => {
                                     return (
                                         <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                             <td className="px-6 py-4">{calculateSerialNumber(index)}</td>
+                                            <td className="px-6 py-4">{value.collegeName}</td>
+                                            <td className="px-6 py-4">{value.batchName}</td>
                                             <td className="px-6 py-4">{value.membership_no}</td>
                                             <td className="px-6 py-4">{value.studName}</td>
                                             <td className="px-6 py-4">{value.msgId}</td>
