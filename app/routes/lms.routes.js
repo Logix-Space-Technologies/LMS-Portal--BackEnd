b11c82b9e7df23b877fe88eba7fc9706883388a8
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-23 12:28:34 +0530

    LMSBE-476 Completed

--- app/routes/lms.routes.js
+++ app/routes/lms.routes.js
@@ -28,6 +28,7 @@ const ClgStaffFirebaseController = require("../controllers/clgStaffFirebaseToken
 const ClgStaffCallBackController = require("../controllers/Whatsapp/callbackAPIClgStaff")
 const ClgStaffViewCallBackController = require("../controllers/Whatsapp/clgStaffwhatsappmsgfeedbackview")
 const ClgStaffViewMsgReceivedController = require("../controllers/Whatsapp/clgstaffwhatsappmsgreceivedview")
+const AdminSearchFeedbackStudController = require("../controllers/Whatsapp/SearchWhatsappFeedbackStud")
 
 // router.post("/", admin.adminRegister)
 router.post("/", AdminController.adminLogin)
@@ -401,6 +402,8 @@ router.post('/viewcallbackAPIClgStaff', ClgStaffViewCallBackController.sendfn)
 router.post('/viewmsgreceivedAPIClgStaff', ClgStaffViewMsgReceivedController.sendfn)
 
 router.post('/clgStaffSearchSession',ClgStaffController.collegeStaffSearchSession)
+
+router.post('/AdminSearchFeedbackStud', AdminSearchFeedbackStudController.sendfn)
 
 
 module.exports = router
