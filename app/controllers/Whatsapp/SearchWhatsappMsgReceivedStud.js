5100cfd809c23e63b0d2e7bd965a1ce037a34ea0
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-23 12:59:26 +0530

    LMSBE-476 Completed

--- app/controllers/Whatsapp/SearchWhatsappMsgReceivedStud.js
+++ app/controllers/Whatsapp/SearchWhatsappMsgReceivedStud.js
@@ -4,10 +4,11 @@ const jwt = require("jsonwebtoken");
 const whatsappmsgreceivedsearchstud = (request, response) => {
     const token = request.headers.token;
     const key = request.headers.key;
+    const searchTerm = request.body.searchTerm
 
     jwt.verify(token, key, (error, decoded) => {
         if (decoded) {
-            db.query("SELECT ce.collegeName, b.batchName, s.membership_no, s.studName, w.id, w.messageId, w.message, w.dateTime, w.studId, c.phone, c.country_code, c.dial_code FROM wtsappmsgreceivedfromstudent w JOIN student s ON s.id = w.studId JOIN wtsappmsgcommon c ON c.messageId = w.messageId JOIN college ce ON ce.id = s.collegeId JOIN batches b ON b.id = s.batchId WHERE (s.studName LIKE ? OR s.membership_no LIKE ? OR ce.collegeName LIKE ? OR b.batchName LIKE ?) AND w.dateTime >= DATE_SUB(NOW(), INTERVAL 1 MONTH) ORDER BY w.dateTime ASC;", (err, res) => {
+            db.query("SELECT ce.collegeName, b.batchName, s.membership_no, s.studName, w.id, w.messageId, w.message, w.dateTime, w.studId, c.phone, c.country_code, c.dial_code FROM wtsappmsgreceivedfromstudent w JOIN student s ON s.id = w.studId JOIN wtsappmsgcommon c ON c.messageId = w.messageId JOIN college ce ON ce.id = s.collegeId JOIN batches b ON b.id = s.batchId WHERE (s.studName LIKE ? OR s.membership_no LIKE ? OR ce.collegeName LIKE ? OR b.batchName LIKE ?) AND w.dateTime >= DATE_SUB(NOW(), INTERVAL 1 MONTH) ORDER BY w.dateTime ASC;", [searchTerm, searchTerm, searchTerm, searchTerm], (err, res) => {
                 if (err) {
                     console.log(err);
                 } else {
