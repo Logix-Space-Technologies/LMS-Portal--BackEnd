541fed6081e91b5f870e5064f9f348e4e0117fd4
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-24 18:37:38 +0530

    LMSBE-479 completed

--- app/controllers/Whatsapp/SearchWhatsappMsgReceivedStud.js
+++ app/controllers/Whatsapp/SearchWhatsappMsgReceivedStud.js
@@ -7,24 +7,31 @@ const whatsappmsgreceivedsearchstud = (request, response) => {
     const searchTerm = request.body.searchTerm
 
     jwt.verify(token, key, (error, decoded) => {
+        if (!searchTerm) {
+            return response.json({ "status": "Search query cannot be empty" })
+        }
         if (decoded) {
             db.query("SELECT ce.collegeName, b.batchName, s.membership_no, s.studName, w.id, w.messageId, w.message, w.dateTime, w.studId, c.phone, c.country_code, c.dial_code FROM wtsappmsgreceivedfromstudent w JOIN student s ON s.id = w.studId JOIN wtsappmsgcommon c ON c.messageId = w.messageId JOIN college ce ON ce.id = s.collegeId JOIN batches b ON b.id = s.batchId WHERE (s.studName LIKE ? OR s.membership_no LIKE ? OR ce.collegeName LIKE ? OR b.batchName LIKE ?) AND w.dateTime >= DATE_SUB(NOW(), INTERVAL 1 MONTH) ORDER BY w.dateTime ASC;", [searchTerm, searchTerm, searchTerm, searchTerm], (err, res) => {
                 if (err) {
                     console.log(err);
                 } else {
-                    const formattedMessageReceivedLog = res.map(messagereceivedlog => ({
-                        ...messagereceivedlog,
-                        dateTime: messagereceivedlog.dateTime.toLocaleString('en-IN', {
-                            timeZone: 'Asia/Kolkata',
-                            day: '2-digit',
-                            month: '2-digit',
-                            year: 'numeric',
-                            hour: '2-digit',
-                            minute: '2-digit',
-                            second: '2-digit'
-                        })
-                    }));
-                    return response.json({ "status": "success", "data": formattedMessageReceivedLog });
+                    if (res.length === 0) {
+                        return response.json({ "status": "No data Found !!" })
+                    } else {
+                        const formattedMessageReceivedLog = res.map(messagereceivedlog => ({
+                            ...messagereceivedlog,
+                            dateTime: messagereceivedlog.dateTime.toLocaleString('en-IN', {
+                                timeZone: 'Asia/Kolkata',
+                                day: '2-digit',
+                                month: '2-digit',
+                                year: 'numeric',
+                                hour: '2-digit',
+                                minute: '2-digit',
+                                second: '2-digit'
+                            })
+                        }));
+                        return response.json({ "status": "success", "data": formattedMessageReceivedLog });
+                    }
                 }
             });
         } else {
--- webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewMsgReceived.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Admin/AdminViewMsgReceived.jsx
@@ -4,6 +4,9 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const AdminViewMsgReceived = () => {
+    const [inputField, setInputField] = useState({
+        "searchTerm": ""
+    });
     const [msgReceivedLogData, setMsgReceivedLogData] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [logsPerPage] = useState(10); // Number of logs per page
@@ -17,6 +20,56 @@ const AdminViewMsgReceived = () => {
     let endPage = Math.min(startPage + rangeSize - 1, lastPage); // Calculate the ending page for the current range
 
     const apiUrl = global.config.urls.api.server + "/api/lms/viewwhatsappmsgreceivedfromstud";
+    const apiUrl2 = global.config.urls.api.server + "/api/lms/AdminSearchWhatsappMsgReceivedStud";
+
+    const inputHandler = (event) => {
+        const { name, value } = event.target;
+        setInputField({ ...inputField, [name]: value });
+    };
+
+    const searchLogs = () => {
+        let currentKey = sessionStorage.getItem("admkey");
+        let token = sessionStorage.getItem("admtoken");
+        setIsLoading(true);
+        const axiosConfig3 = {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                "Access-Control-Allow-Origin": "*",
+                "token": token,
+                "key": currentKey
+            }
+        };
+        const data = {
+            "searchTerm": inputField.searchTerm
+        }
+        axios.post(apiUrl2, data, axiosConfig3).then((response) => {
+            if (response.data.status === "Search query cannot be empty") {
+                setIsLoading(false)
+                setTimeout(() => {
+                    alert(response.data.status)
+                    getData()
+                }, 500)
+            } else if (response.data.data) {
+                setMsgReceivedLogData(response.data.data);
+                setInputField({ "searchTerm": "" })
+                setIsLoading(false);
+            } else if (response.data.status === "Unauthorized User!!") {
+                navigate("/")
+                sessionStorage.clear()
+            } else if (!response.data.data) {
+                setIsLoading(false);
+                setInputField({ "searchTerm": "" })
+                setTimeout(() => {
+                    alert("No Logs Found !!")
+                    getData();
+                }, 500)
+            } else {
+                alert(response.data.status)
+                setInputField({ "searchTerm": "" })
+                setIsLoading(false);
+            }
+        })
+    };
 
     const getData = () => {
         let axiosConfig = {
@@ -71,6 +124,25 @@ const AdminViewMsgReceived = () => {
                 <Navbar />
                 <br />
                 <strong>Admin View Student Whatsapp Message Received Log</strong><br /><br />
+                <div className="row">
+                    <div className="col">
+                        <div className="input-group">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Search by batch name, college name, student name or membership no..."
+                                value={inputField.searchTerm}
+                                onChange={inputHandler}
+                                name="searchTerm"
+                            />
+                        </div>
+                        <br></br>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button onClick={searchLogs} className="btn btn-warning">Search</button>
+                        </div>
+                        <br />
+                    </div>
+                </div>
                 <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                     {isLoading ? <div className="flex justify-center items-center h-full">
                         <div className="text-center py-20">
