60635f7da35953d33ef58e245f315a1f8ae0428d
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-24 19:18:21 +0530

    LMSBE-479 completed

--- app/controllers/Whatsapp/SearchWhatsappMsgReceivedStud.js
+++ app/controllers/Whatsapp/SearchWhatsappMsgReceivedStud.js
@@ -11,7 +11,7 @@ const whatsappmsgreceivedsearchstud = (request, response) => {
             return response.json({ "status": "Search query cannot be empty" })
         }
         if (decoded) {
-            db.query("SELECT ce.collegeName, b.batchName, s.membership_no, s.studName, w.id, w.messageId, w.message, w.dateTime, w.studId, c.phone, c.country_code, c.dial_code FROM wtsappmsgreceivedfromstudent w JOIN student s ON s.id = w.studId JOIN wtsappmsgcommon c ON c.messageId = w.messageId JOIN college ce ON ce.id = s.collegeId JOIN batches b ON b.id = s.batchId WHERE (s.studName LIKE ? OR s.membership_no LIKE ? OR ce.collegeName LIKE ? OR b.batchName LIKE ?) AND w.dateTime >= DATE_SUB(NOW(), INTERVAL 1 MONTH) ORDER BY w.dateTime ASC;", [searchTerm, searchTerm, searchTerm, searchTerm], (err, res) => {
+            db.query("SELECT ce.collegeName, b.batchName, s.membership_no, s.studName, w.id, w.messageId, w.message, w.dateTime, w.studId, c.phone, c.country_code, c.dial_code FROM wtsappmsgreceivedfromstudent w JOIN student s ON s.id = w.studId JOIN wtsappmsgcommon c ON c.messageId = w.messageId JOIN college ce ON ce.id = s.collegeId JOIN batches b ON b.id = s.batchId WHERE (s.studName LIKE ? OR s.membership_no LIKE ? OR ce.collegeName LIKE ? OR b.batchName LIKE ?) AND w.dateTime >= DATE_SUB(NOW(), INTERVAL 1 MONTH) ORDER BY w.dateTime ASC;", [`%${searchTerm}%`, `%${searchTerm}%`, `%${searchTerm}%`, `%${searchTerm}%`], (err, res) => {
                 if (err) {
                     console.log(err);
                 } else {
