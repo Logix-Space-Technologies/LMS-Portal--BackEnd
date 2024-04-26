2eb3fe30098eceb4fcfa06fc8c14c7573649b48c
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-26 18:33:07 +0530

    LMSBE-480 msg received status updated

--- app/controllers/Whatsapp/callbackAPI.js
+++ app/controllers/Whatsapp/callbackAPI.js
@@ -89,11 +89,19 @@ function handleSentMessage(payload) {
 
 
 
-    db.query("UPDATE whatsappmsgfeedback SET sentStatus = 1, sentDate = ? WHERE msgId = ?", [mysqlTimestamp, payload.gsId], function (err) {
+    db.query("UPDATE whatsappmsgfeedback SET sentStatus = 1, sentDate = ? WHERE msgId = ?", [mysqlTimestamp, payload.gsId], function (err, res) {
         if (err) {
-            console.error('Error updating sent statsus for msgId:', payload.gsId, '; Error:', err);
+            console.error('Error updating sent status for msgId in whatsappmsgfeedback:', payload.gsId, '; Error:', err);
+        } else if (res.affectedRows === 0) {
+            db.query("UPDATE whatsappmsgfeedbackclgstaff SET sentStatus = 1, sentDate = ? WHERE msgId = ?", [mysqlTimestamp, payload.gsId], function (err) {
+                if (err) {
+                    console.error('Error updating sent statsus for msgId in whatsappmsgfeedbackclgstaff:', payload.gsId, '; Error:', err);
+                } else {
+                    console.log('Sent status updated successfully for msgId in whatsappmsgfeedbackclgstaff:', payload.gsId);
+                }
+            });
         } else {
-            console.log('Sent status updated successfully for msgId:', payload.gsId);
+            console.log('Sent status updated successfully for msgId in whatsappmsgfeedback:', payload.gsId);
         }
     });
 
@@ -117,11 +125,19 @@ function handleDeliveredMessage(payload) {
 
 
     // Example: Update deliveryStatus for the message
-    db.query("UPDATE whatsappmsgfeedback SET deliveryStatus = 1, deliveryDate = ? WHERE msgId = ?", [mysqlTimestamp, payload.gsId], function (err) {
+    db.query("UPDATE whatsappmsgfeedback SET deliveryStatus = 1, deliveryDate = ? WHERE msgId = ?", [mysqlTimestamp, payload.gsId], function (err, res) {
         if (err) {
-            console.error('Error updating deliveryStatus for msgId:', payload.gsId, '; Error:', err);
+            console.error('Error updating deliveryStatus for msgId in whatsappmsgfeedback:', payload.gsId, '; Error:', err);
+        } else if (res.affectedRows === 0) {
+            db.query("UPDATE whatsappmsgfeedbackclgstaff SET deliveryStatus = 1, deliveryDate = ? WHERE msgId = ?", [mysqlTimestamp, payload.gsId], function (err) {
+                if (err) {
+                    console.error('Error updating deliveryStatus for msgId in whatsappmsgfeedbackclgstaff:', payload.gsId, '; Error:', err);
+                } else {
+                    console.log('Delivery status updated successfully for msgId in whatsappmsgfeedbackclgstaff:', payload.gsId);
+                }
+            });
         } else {
-            console.log('Delivery status updated successfully for msgId:', payload.gsId);
+            console.log('Delivery status updated successfully for msgId in whatsappmsgfeedback:', payload.gsId);
         }
     });
 }
@@ -145,11 +161,19 @@ function handleReadMessage(payload) {
     const mysqlTimestamp = getCurrentTimeForGMT530();
 
 
-    db.query("UPDATE whatsappmsgfeedback SET readStatus = 1, readDateTime = ? WHERE msgId = ?", [mysqlTimestamp, payload.gsId], function (err) {
+    db.query("UPDATE whatsappmsgfeedback SET readStatus = 1, readDateTime = ? WHERE msgId = ?", [mysqlTimestamp, payload.gsId], function (err, res) {
         if (err) {
-            console.error('Error updating read Status for msgId:', payload.gsId, '; Error:', err);
+            console.error('Error updating read Status for msgId in whatsappmsgfeedback:', payload.gsId, '; Error:', err);
+        } else if (res.affectedRows === 0) {
+            db.query("UPDATE whatsappmsgfeedbackclgstaff SET readStatus = 1, readDateTime = ? WHERE msgId = ?", [mysqlTimestamp, payload.gsId], function (err) {
+                if (err) {
+                    console.error('Error updating read Status for msgId in whatsappmsgfeedbackclgstaff:', payload.gsId, '; Error:', err);
+                } else {
+                    console.log('Read status updated successfully for msgId in whatsappmsgfeedbackclgstaff:', payload.gsId);
+                }
+            });
         } else {
-            console.log('Read status updated successfully for msgId:', payload.gsId);
+            console.log('Read status updated successfully for msgId in whatsappmsgfeedback:', payload.gsId);
         }
     });
 
@@ -169,9 +193,9 @@ function handleBillingEvent(data) {
 //     return formatted;
 // }
 function convertToMySQLTimestamp(timestamp) {
-    console.log('iNSIDE  tIME STAMP ');
+    // console.log('iNSIDE  tIME STAMP ');
 
-    console.log("Original timestamp: ", timestamp);
+    // console.log("Original timestamp: ", timestamp);
 
     // Create a Date object from the timestamp
     const date = new Date(timestamp);
@@ -196,19 +220,36 @@ function handleMessageReceived(data) {
     //console.log('tEST tIME STAMP ');
 
     //console.log('Handling message received event:', data.timestamp);
-    console.log("Before calling convertToMySQLTimestamp");
+    // console.log("Before calling convertToMySQLTimestamp");
 
     // Convert timestamp to MySQL TIMESTAMP format
     const mysqlTimestamp = convertToMySQLTimestamp(data.timestamp);
 
-    console.log("formatted timestamp => ", mysqlTimestamp)
-    console.log(mysqlTimestamp)
+    // console.log("formatted timestamp => ", mysqlTimestamp)
+    // console.log(mysqlTimestamp)
 
 
     let studentPhno = data.payload.sender.phone.replace(/^\+91\s?|^\91\s?/, '');
     db.query("SELECT * FROM `student` WHERE `studPhNo` = ?", [studentPhno], (err, res) => {
         if (err) {
             console.error('Error updating readStatus:', err);
+        } else if (res.length === 0) {
+            let phNo = data.payload.sender.phone.replace(/^\+91\s?|^\91\s?/, '');
+            db.query("SELECT * FROM `college_staff` WHERE `phNo` = ?", [phNo], (err, clgstaffres) => {
+                if (err) {
+                    console.error('Error updating readStatus:', err);
+                } else {
+                    let clgstaffId = clgstaffres[0].id;
+                    // Use the converted timestamp
+                    db.query("INSERT INTO `wtsappmsgreceivedfromclgstaff`(`messageId`, `clgstaffId`, `message`, `dateTime`) VALUES (?,?,?,?)", [data.payload.id, clgstaffId, data.payload.payload.text, mysqlTimestamp], (err) => {
+                        if (err) console.error('Error updating readStatus:', err);
+                    });
+                    // Use the converted timestamp
+                    db.query("INSERT INTO `wtsappmsgcommon`(`messageId`, `dateTime`, `name`, `phone`, `country_code`, `dial_code`, `text`) VALUES (?,?,?,?,?,?,?)", [data.payload.id, mysqlTimestamp, data.payload.sender.name, data.payload.sender.phone, data.payload.sender.country_code, data.payload.sender.dial_code, data.payload.payload.text], (err) => {
+                        if (err) console.error('Error updating readStatus:', err);
+                    });
+                }
+            });
         } else {
             let studId = res[0].id;
             // Use the converted timestamp
