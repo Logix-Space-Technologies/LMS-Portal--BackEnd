8560c37f17f66805c60c4d75ac0a9723564b5a15
Glenngt <glenn.mattackal@gmail.com>
2024-04-30 14:22:38 +0530

    Changes

--- app/models/college.model.js
+++ app/models/college.model.js
@@ -303,7 +303,7 @@ College.viewPerformanceOfStudents = (collegeId, result) => {
 College.searchPerformanceOfStudents = (collegeId,search, result) => {
     const searchTerm = '%' + search + '%'
     db.query("SELECT batchName, membership_no, studName, SUM(score) AS score, SUM(totalScore) AS totalScore FROM studentTaskScore WHERE CollegeId=? AND dueDate < CURRENT_DATE AND (batchName LIKE ? OR membership_no = ? OR studName LIKE ?) GROUP BY batchName, studentId, studName ORDER BY batchName, score DESC;",
-        [collegeId,searchTerm, searchTerm, searchTerm],
+        [collegeId,searchTerm, search, searchTerm],
         (err, res) => {
             if (err) {
                 console.log("Error : ", err)
