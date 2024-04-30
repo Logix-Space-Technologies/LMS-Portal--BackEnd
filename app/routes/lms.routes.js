de30ff80b19bb529f75db45ae33ea28a03247890
Glenngt <glenn.mattackal@gmail.com>
2024-04-30 12:10:43 +0530

    Update Task Score Completed

--- app/routes/lms.routes.js
+++ app/routes/lms.routes.js
@@ -421,4 +421,6 @@ router.post('/generatebatchwisestudListDummy', StudentController.generateListOfB
 router.post('/generatebatchwiseattendanceListDummy', StudentController.generateBatchWiseAttendanceListDummy)
 //Dummy Ends
 
+router.post('/updateSubmittedTaskScore',SubmitTaskController.updateTaskScore)
+
 module.exports = router
