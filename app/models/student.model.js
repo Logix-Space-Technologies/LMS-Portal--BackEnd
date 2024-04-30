484f6160c57ff7988ab17ad61b88d3c0f766b28d
Sree1710 <sreelekshmisl1710@gmail.com>
2024-04-30 18:31:06 +0530

    test

--- app/models/student.model.js
+++ app/models/student.model.js
@@ -1538,6 +1538,18 @@ Student.viewPerformanceScore = (studId, result) => {
 
 }
 
+Student.viewOverallPerformanceStudWise = (studId, result) => {
+    db.query("SELECT COUNT(taskId) AS totalTasksAssigned, COUNT(submitTaskId) AS totalTasksSubmitted, SUM(CASE WHEN dueDate < CURRENT_DATE AND subDate IS NOT NULL THEN score ELSE 0 END) AS score, SUM(CASE WHEN dueDate < CURRENT_DATE THEN totalScore ELSE 0 END) AS totalScore, COUNT(CASE WHEN dueDate < CURRENT_DATE AND lateSubDate IS NOT NULL THEN taskId END) AS tasksSubmittedLate, COUNT(CASE WHEN subDate IS NOT NULL AND lateSubDate IS NULL THEN taskId END) AS tasksSubmittedOnTime FROM studentTaskScore WHERE studentId = ?", [studId], (err, res) => {
+        if (err) {
+            console.log("Error: ", err)
+            return result(err, null)
+        } else {
+            console.log(res)
+            return result(null, res)
+        }
+    })
+}
+
 
 Student.generateTaskWiseScoreList = (taskId, result) => {
     let query = "SELECT taskName, membership_no, studName, score, totalScore FROM studentTaskScore WHERE taskId = ? GROUP BY studName, membership_no, taskName, score, totalScore ORDER BY score DESC"
