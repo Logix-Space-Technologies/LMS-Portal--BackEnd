c955c3a83998669366b3d530fda6b6acd76c37ac
Sree1710 <sreelekshmisl1710@gmail.com>
2024-05-01 14:29:28 +0530

    LMSBE-482 filter completed

--- app/models/student.model.js
+++ app/models/student.model.js
@@ -889,7 +889,7 @@ Student.studentNotificationView = (studId, result) => {
 
 Student.viewSession = (batchId, result) => {
     db.query(
-        "SELECT s.id,s.sessionName, s.date, s.time, s.type, s.remarks, s.venueORlink,t.trainerName FROM sessiondetails s JOIN trainersinfo t ON s.trainerId = t.id  WHERE (s.date >= DATE_SUB(NOW(), INTERVAL 2 MONTH)) AND s.deleteStatus = 0 AND s.isActive = 1 AND s.cancelStatus = 0 AND s.batchId = ? ORDER BY s.date DESC;",
+        "SELECT s.id,s.sessionName, s.date, s.time, s.type, s.remarks, s.venueORlink,t.trainerName FROM sessiondetails s JOIN trainersinfo t ON s.trainerId = t.id  WHERE (s.date >= DATE_SUB(NOW(), INTERVAL 4 MONTH)) AND s.deleteStatus = 0 AND s.isActive = 1 AND s.cancelStatus = 0 AND s.batchId = ? ORDER BY s.date DESC;",
         [batchId],
         (err, res) => {
             if (err) {
