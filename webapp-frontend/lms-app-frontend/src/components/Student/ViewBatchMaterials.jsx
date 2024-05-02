6594c81932334beb118dfce06469aceff674cbb9
Glenngt <glenn.mattackal@gmail.com>
2024-05-02 15:00:44 +0530

    Changes

--- webapp-frontend/lms-app-frontend/src/components/Student/ViewBatchMaterials.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Student/ViewBatchMaterials.jsx
@@ -90,9 +90,7 @@ const MaterialView = () => {
                     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                       <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                          <th scope="col" className="px-6 py-3">
-                            Batch Name
-                          </th>
+                        
                           <th scope="col" className="px-6 py-3">
                             Material Name
                           </th>
@@ -121,9 +119,7 @@ const MaterialView = () => {
                           ) : (
                             currentMaterials.map((material, index) => (
                               <tr key={index} className={index % 2 === 0 ? 'even:bg-gray-50 even:dark:bg-gray-800' : 'odd:bg-white odd:dark:bg-gray-900 border-b dark:border-gray-700'}>
-                                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                  {material.batchName}
-                                </td>
+                                
                                 <td className="px-6 py-4">
                                   {material.fileName}
                                 </td>
