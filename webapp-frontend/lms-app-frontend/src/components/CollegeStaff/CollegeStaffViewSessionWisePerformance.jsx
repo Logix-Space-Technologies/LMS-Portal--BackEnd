7b8b59903fbddf02a1673d16af7551d48d48c828
Sree1710 <sreelekshmisl1710@gmail.com>
2024-05-04 19:12:26 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewSessionWisePerformance.jsx
+++ webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewSessionWisePerformance.jsx
@@ -177,7 +177,7 @@ const CollegeStaffViewSessionWisePerformance = () => {
                     <div className="flex flex-wrap -mx-4">
                         <div className="w-full px-4">
                             <div className="flex justify-between items-center mt-8 ml-4 mb-4">
-                                {key === 'lmsapp' ? <h2 className="text-lg font-bold">Admin View Performance Of Session {`- ${SessionName}`} </h2> : (key === 'lmsappclgstaff' ? <h2 className="text-lg font-bold">College Staff View Performance Session-Wise </h2> : <h2 className="text-lg font-bold">Admin Staff View Performance Session-Wise </h2>)}
+                                {key === 'lmsapp' ? <h2 className="text-lg font-bold">Admin View Performance Of Session {`- ${SessionName}`} </h2> : (key === 'lmsappclgstaff' ? <h2 className="text-lg font-bold">College Staff View Performance Of Session {`- ${SessionName}`} </h2> : <h2 className="text-lg font-bold">Admin Staff View Performance Of Session {`- ${SessionName}`} </h2>)}
                                 <div className="flex space-x-4">
                                     <button type='button' onClick={() => navigate(-1)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Back</button>
                                     <button type='button' onClick={() => generatePDF()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Download PDF</button>
