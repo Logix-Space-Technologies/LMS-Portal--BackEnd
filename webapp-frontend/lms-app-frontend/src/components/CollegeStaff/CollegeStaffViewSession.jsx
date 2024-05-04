ab7550165ef7ef59e94cac09acf012d3038c505e
Sree1710 <sreelekshmisl1710@gmail.com>
2024-05-04 19:36:39 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewSession.jsx
+++ webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewSession.jsx
@@ -211,7 +211,7 @@ const CollegeStaffViewSession = () => {
                 <br />
                 <br />
                 <div className="flex justify-between items-center mt-8 ml-4 mb-4">
-                    <h2 className="text-lg font-bold">College Staff View Session</h2>
+                    <h2 className="text-lg font-bold">View All Session</h2>
                     <Link to="/collegeStaffViewBatch" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{ marginRight: '20px' }}>Back</Link>
                 </div>
                 <br /><br />
