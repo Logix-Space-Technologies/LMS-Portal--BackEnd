de48ee65ac39cb74600c10218f0e6d057d398a78
Sree1710 <sreelekshmisl1710@gmail.com>
2024-05-05 15:39:33 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewAllStudents.jsx
+++ webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffViewAllStudents.jsx
@@ -156,7 +156,6 @@ const CollegeStaffViewAllStudents = () => {
       {loading ? <div className="col-12 text-center">Loading...</div> : <div className="relative overflow-x-auto shadow-md sm:rounded-lg"><table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="px-6 py-3">Batch Name</th>
             <th scope="col" className="px-6 py-3"></th>
             <th scope="col" className="px-6 py-3">Name</th>
             <th scope="col" className="px-6 py-3">Department</th>
@@ -174,7 +173,6 @@ const CollegeStaffViewAllStudents = () => {
         <tbody>
           {currentStudents.map((student, index) => {
             return <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <td className="px-6 py-4">{student.batchName}</td>
               <td className="px-6 py-4">
                 {student.studProfilePic && <img className="w-10 h-10 rounded-full" src={student.studProfilePic} alt={student.studName} />}
               </td>
