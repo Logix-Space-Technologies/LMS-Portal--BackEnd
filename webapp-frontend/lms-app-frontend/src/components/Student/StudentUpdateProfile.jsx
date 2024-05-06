9ac4d6dc914ffeb3d6e2b34d35c5771d1dff0861
Sree1710 <sreelekshmisl1710@gmail.com>
2024-05-06 16:44:25 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/Student/StudentUpdateProfile.jsx
+++ webapp-frontend/lms-app-frontend/src/components/Student/StudentUpdateProfile.jsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from '../Admin/Navbar';
 import AdmStaffNavBar from '../AdminStaff/AdmStaffNavBar';
-import StudNavBar from './StudNavBar';
 
 
 
@@ -253,7 +252,7 @@ const StudentUpdateProfile = () => {
 
     return (
         <div>
-            {key === 'lmsappstud' ? <StudNavBar/> : (key === 'lmsapp' ? <Navbar /> : <AdmStaffNavBar />)}
+            {key === 'lmsappstud' ? '' : (key === 'lmsapp' ? <Navbar /> : <AdmStaffNavBar />)}
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12 mb-4 mb-sm-5">
