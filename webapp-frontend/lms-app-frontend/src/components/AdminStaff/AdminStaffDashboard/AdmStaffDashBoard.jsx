1ea0f40a20096f5ab788bd4a57fa7e214e45df13
Sree1710 <sreelekshmisl1710@gmail.com>
2024-05-07 14:41:36 +0530

    test

--- webapp-frontend/lms-app-frontend/src/components/AdminStaff/AdminStaffDashboard/AdmStaffDashBoard.jsx
+++ webapp-frontend/lms-app-frontend/src/components/AdminStaff/AdminStaffDashboard/AdmStaffDashBoard.jsx
@@ -7,7 +7,6 @@ import AdmStaffNavBar from '../AdmStaffNavBar'
 
 const AdmStaffDashBoard = () => {
   return (
-
     <div>
       <AdmStaffNavBar/>
       <div className="container-xxl position-relative bg-white d-flex p-0">
@@ -23,7 +22,6 @@ const AdmStaffDashBoard = () => {
         </div>
       </div>
     </div>
-
   )
 }
 
