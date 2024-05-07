1aaa8b108b50e9fbf88ececf779cd0745af731f0
Sree1710 <sreelekshmisl1710@gmail.com>
2024-05-07 15:27:14 +0530

    LMSBE-489 bug fixed

--- webapp-frontend/lms-app-frontend/src/components/CollegeStaff/ClgStaffViewScoreStudentWise.jsx
+++ webapp-frontend/lms-app-frontend/src/components/CollegeStaff/ClgStaffViewScoreStudentWise.jsx
@@ -93,7 +93,7 @@ const ClgStaffViewScoreStudentWise = () => {
 
     return (
         <div>
-            {key === 'lmsappclgstaff' ? <ClgStaffNavbar /> : (key === 'lmsapp' ? <Navbar /> : <AdmStaffNavBar />)}
+            {key === 'lmsapp' ? <Navbar /> : (key === 'lmsappadmstaff' ? <AdmStaffNavBar /> : <ClgStaffNavbar />)}
             {/* ====== Table Section Start */}
             <section className="bg-gray-100 dark:bg-dark py-20 lg:py-[120px]">
                 <div className="container mx-auto">
