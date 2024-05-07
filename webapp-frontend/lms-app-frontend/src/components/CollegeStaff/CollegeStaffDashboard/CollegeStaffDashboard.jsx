a26176d5ffa4039f181aa9c3b8ec03c8daba391b
Sree1710 <sreelekshmisl1710@gmail.com>
2024-05-07 15:15:42 +0530

    LMSBE-489 test

--- webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffDashboard/CollegeStaffDashboard.jsx
+++ webapp-frontend/lms-app-frontend/src/components/CollegeStaff/CollegeStaffDashboard/CollegeStaffDashboard.jsx
@@ -4,20 +4,25 @@ import CollegeStaffFooter from './CollegeStaffFooter'
 import CollegeStaffViewProfile from './CollegeStaffViewProfile'
 import CollegeStaffSideBar from './CollegeStaffSideBar'
 import { Link } from 'react-router-dom'
+import ClgStaffNavbar from '../ClgStaffNavbar';
 
 
 const CollegeStaffDashboard = () => {
   return (
-    <div className="container-xxl position-relative bg-white d-flex p-0">
-      <div className="content">
-        <CollegeStaffHeader />
-        <CollegeStaffSideBar />
-        <CollegeStaffViewProfile />
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <CollegeStaffFooter />
+    <div>
+      <ClgStaffNavbar />
+      <div className="container-xxl position-relative bg-white d-flex p-0">
+        <div className="content">
+          {/* <CollegeStaffHeader />
+        <CollegeStaffSideBar /> */}
+          <br />
+          <h1 className="text-center" style={{fontWeight:'bold', fontSize:'20px'}}>Batch In-Charge Dashboard</h1>
+          <CollegeStaffViewProfile />
+          <br></br>
+          <br />
+          <br />
+          <CollegeStaffFooter />
+        </div>
       </div>
     </div>
   )
