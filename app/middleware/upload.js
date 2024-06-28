08e059565f1130ace3a15cd2afabdeeded50eb06
Animesh Thomas <animeshthomas262@gmail.com>
2024-06-28 20:17:09 +0530

    done

--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -1,6 +1,9 @@
 const multer = require('multer');
 const path = require('path');
 
+// List of allowed image extensions
+const allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+
 // Multer configuration for file uploads with unique filenames
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -18,11 +21,10 @@ const upload = multer({
     storage: storage,
     limits: { fileSize: 2 * 1024 * 1024 }, // 2 MB file size limit
     fileFilter: (req, file, cb) => {
-        if (file.mimetype.startsWith('image/')) {
+        const ext = path.extname(file.originalname).toLowerCase();
+        if (file.mimetype.startsWith('image/') || allowedExtensions.includes(ext)) {
             cb(null, true);
         } else {
-            console.log(file.path)
-            console.log(file.mimetype)
             cb(new Error('Only image files are allowed!'), false);
         }
     }
