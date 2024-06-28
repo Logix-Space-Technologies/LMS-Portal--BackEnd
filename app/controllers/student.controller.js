3d6f136ab2b8a4cfbdcfab3cadbdd1b71d27831e
Animesh Thomas <animeshthomas262@gmail.com>
2024-06-28 20:42:26 +0530

    done

--- app/controllers/student.controller.js
+++ app/controllers/student.controller.js
@@ -656,31 +656,32 @@ exports.profileUpdateStudent = (request, response) => {
 
 
 
+
 exports.profileUpdateStudentMobile = async (req, res) => {
     try {
         const { id, studName, admNo, rollNo, studDept, course, studPhNo, aadharNo } = req.body;
         const file1 = req.file;
 
         if (!file1) {
-            return res.status(400).json({ error: 'No image uploaded Test' });
+            return res.status(400).json({ error: 'No image uploaded' });
         }
 
-        // const fileStream1 = fs.createReadStream(file.path);
-        // const uploadParams = {
-        //     Bucket: process.env.AWS_BUCKET_NAME,
-        //     Key: student-profiles/${file.filename},
-        //     Body: fileStream,
-        //     ContentType: file.mimetype,
-        // };
+        const fileStream1 = fs.createReadStream(file1.path);
+        const uploadParams = {
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: `student-profiles/${file1.filename}`,
+            Body: fileStream1,
+            ContentType: file1.mimetype,
+        };
 
-        // const command = new PutObjectCommand(uploadParams);
+        const command = new PutObjectCommand(uploadParams);
 
         try {
-            // await s3Client.send(command);
-            // const imageUrl = https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/student-profiles/${file.filename};
+            const data = await s3Client.send(command);
+            const imageUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/student-profiles/${file1.filename}`;
 
-            // // Clean up the local file after upload
-            // fs.unlinkSync(file.path);
+            // Clean up the local file after upload
+            fs.unlinkSync(file1.path);
 
             // Log the received data
             console.log('Form Data:');
