92a009f75ed2632eae98cda524359491014e036c
anishpdm <anish.vilayil.s@gmail.com>
2025-03-29 12:41:46 +0530

    email change

--- sendEmail.js
+++ sendEmail.js
@@ -1,54 +1,39 @@
 require('dotenv').config({ path: '../../.env' });
-const nodemailer = require('nodemailer');
-const sgMail = require('@sendgrid/mail');
-//npm install @sendgrid/mail
-// Set your SendGrid API Key
-sgMail.setApiKey(process.env.SendGridKey);
+const { SendMailClient } = require('zeptomail');
+
+// Initialize ZeptoMail client
+const zeptoClient = new SendMailClient({
+  url: 'https://api.zeptomail.in/',
+  token: `Zoho-enczapikey ${process.env.ZEPTO_API_TOKEN}`,
+});
 
 async function sendEmail(to, subject, htmlContent, textContent) {
-  // let transporter = nodemailer.createTransport({
-  //   host: 'smtp.zoho.in',
-  //   port: 465,
-  //   secure: true,
-  //   auth: {
-  //     user: process.env.ZOHO_USER,
-  //     pass: process.env.ZOHO_PASSWORD,
-  //   },
-  // });
-
-  // let mailOptions = {
-  //   from: `"LINK UR CODES TEAM" <${process.env.ZOHO_USER}>`,
-  //   to: to,
-  //   subject: subject,
-  //   html: htmlContent, // HTML content
-  // };
-  const msg = {
-    to: to, // Recipient's email
-    from: 'Link Ur Codes Team <team@linkurcodes.com>', // Sender's email
+  const emailData = {
+    from: {
+      address: 'team@linkurcodes.com',
+      name: 'Link Ur Codes Team',
+    },
+    to: [
+      {
+        email_address: {
+          address: to,
+          name: '', // You can add recipient's name here if available
+        },
+      },
+    ],
     subject: subject,
-    text: textContent,
-    html: htmlContent,
+    htmlbody: htmlContent,
+    textbody: textContent,
   };
-  // Send the email
-  sgMail.send(msg)
-    .then(() => {
-      console.log('Email has sent');
-    })
-    .catch((error) => {
-      console.log("Api Key => "+process.env.SendGridKey)
-      console.error(error);
-    });
 
-  // transporter.sendMail(mailOptions, (error, info) => {
-  //   if (error) {
-  //     return console.log(error);
-  //   }
-  //   console.log('Message sent: %s', info.messageId);
-  // });
+  try {
+    const response = await zeptoClient.sendMail(emailData);
+    console.log('Email sent successfully:', response);
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
 }
 
-
-
 module.exports = {
-  sendEmail
+  sendEmail,
 };
