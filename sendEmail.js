e79639cc7773e79f0f74bea507f8baf6aa55beef
anishpdm <anish.vilayil.s@gmail.com>
2025-03-29 12:53:24 +0530

    email change

--- sendEmail.js
+++ sendEmail.js
@@ -3,7 +3,7 @@ const { SendMailClient } = require('zeptomail');
 
 // Initialize ZeptoMail client
 const zeptoClient = new SendMailClient({
-  url: 'api.zeptomail.in',
+  url: 'https://api.zeptomail.in/',
   token: `Zoho-enczapikey ${process.env.ZEPTO_API_TOKEN}`,
 });
 
