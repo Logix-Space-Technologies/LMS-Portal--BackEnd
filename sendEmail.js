3466b768aade8021cb9a251f5f1f94ec3954a3de
anishpdm <anish.vilayil.s@gmail.com>
2025-03-29 13:14:09 +0530

    email

--- sendEmail.js
+++ sendEmail.js
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+require('dotenv').config();
 
 // require('dotenv').config({ path: '../../.env' });
 const { SendMailClient } = require('zeptomail');
